4892b56da8bfe999d10efea98dc2e1ea42e62283
rgreen <rgreen>
2001-04-23 13:11:50 +0000

    Initial import of schema r18

---
+++ flex/ApplicationCode/DDL/flex_ddl.sql
@@ -0,0 +1,13 @@
+-- E:\flex_ddl.sql
+--
+-- Generated for Oracle 8.1 on Mon Apr 23  12:12:22 2001 by Server Generator 6.5.40.3.0
+
+
+SPOOL flex_ddl.lst
+
+@@flex_ddl.tab
+@@flex_ddl.ind
+@@flex_ddl.con
+@@flex_ddl.sqs
+
+SPOOL OFF
