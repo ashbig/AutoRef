51956683910588a40c283c7bb84fe12f4d0623f9
rgreen <rgreen>
2001-04-30 20:32:50 +0000

    DDL changes from r19 to r20 -	SAMPLELINEAGE: both SAMPLEID_FROM and SAMPLEID_TO are mandatory -	FLEXSEQUENCE: added DATESTAMP date mandatory -	FILEREFERENCE: included optional fk to RESULT -	CONSTRUCTDESIGN: removed CDNALIBRARYID and fk; CDNALIBRARY choice becomes an extended attribute of the PROCESSEXECUTION object, under extrainformation.  DEFAULT is set in PROCESSPROTOCOL; options are listed (still) in the CDNA library table.

--- flex031403/ApplicationCode/DDL/flex_ddl.sql
+++ flex031403/ApplicationCode/DDL/flex_ddl.sql
@@ -1,6 +1,6 @@
 -- E:\flex_ddl.sql
 --
--- Generated for Oracle 8.1 on Wed Apr 25  14:06:49 2001 by Server Generator 6.5.40.3.0
+-- Generated for Oracle 8.1 on Mon Apr 30  19:38:21 2001 by Server Generator 6.5.40.3.0
 
 
 SPOOL flex_ddl.lst
