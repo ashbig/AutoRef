0fc6f282b6c77bf3a35fee38e54bb06b4de5f86e
rgreen <rgreen>
2001-05-01 12:31:34 +0000

    Minor updates to r20

--- flex031403/ApplicationCode/DDL/flex_ddl.sql
+++ flex031403/ApplicationCode/DDL/flex_ddl.sql
@@ -1,6 +1,6 @@
 -- E:\flex_ddl.sql
 --
--- Generated for Oracle 8.1 on Mon Apr 30  19:38:21 2001 by Server Generator 6.5.40.3.0
+-- Generated for Oracle 8.1 on Tue May 01  11:28:42 2001 by Server Generator 6.5.40.3.0
 
 
 SPOOL flex_ddl.lst
