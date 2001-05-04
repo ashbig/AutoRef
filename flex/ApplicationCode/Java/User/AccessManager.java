c3ba420f7a68bb1f017d44fae4221e3c9915cd8b
dongmei_zuo <dongmei_zuo>
2001-05-04 18:37:52 +0000

    modified some methods and tested.

--- flex/ApplicationCode/Java/User/AccessManager.java
+++ flex/ApplicationCode/Java/User/AccessManager.java
@@ -1,14 +1,22 @@
+package flex.ApplicationCode.Java.User;
 
-//import java.util.*;
+import flex.ApplicationCode.Java.database.*;
+import java.sql.*;
+import java.util.*;
+
 /**
  * This class manages user login.
  * It verifies user authentification and manages user groups
  * @author Wendy Mar 
+ *
+ * Revision: 05-04-2001 by dzuo
+ *           Modified authenticate method and tested.
  */
 
 public class AccessManager
 {
 	private static AccessManager _instance = null;
+	private String username;
 	private String password;
 	private String group;
 
@@ -29,7 +37,7 @@ public class AccessManager
  	private AccessManager(){} 
 
 	/**
-	 * Set user password.
+	 * change user password.
 	 *
 	 * @param String pw The User password as String.
 	 */
@@ -39,19 +47,60 @@ public class AccessManager
 	}
 
 	/**
-	 * This method retrieves the user password from
-	 * the user profile table for a given user id.
+	 * This method query the userprofile table to determine
+	 * whether an user has already existed during user login
 	 *
-	 * @param userid the userid as integer.
+	 * @param name The username as String.
+	 * @return boolean An Yes or No to indicate whether the user
+	 * name exist in the userprofile table.
+	 */
+
+	public boolean userExist(String name) throws FlexDatabaseException
+	{
+		boolean isExist = false;
+		String sql;
+		ResultSet rs = null;
+		sql = "SELECT username FROM Userprofile"
+			+ " WHERE username = '" + name+"'";
+
+		try {
+		   DatabaseTransaction t = DatabaseTransaction.getInstance();
+		   rs = t.getResultset(sql);
+		   isExist = rs.next(); 
+		}catch (SQLException sqlex) {
+			sqlex.printStackTrace();
+		}
+
+		return isExist;
+	}
+
+
+	/**
+	 * This method retrieves the user password from
+	 * the userprofile table for a given username.
+	 * pre-condition: the given username must exist
+	 *
+	 * @param username.
 	 * @return the user password
 	 */
-	private String getPassword(int userid) {
-		String pw = null; //password
+	private String getPassword(String username) throws FlexDatabaseException {
+		String pswd = null; //password
 		String sql;
-		sql = "SELECT user_password FROM Userprofile"
-			+ " WHERE user_id = " + userid;
-		// to be implemented
-		return pw;
+		ResultSet rs = null;
+		sql = "SELECT userpassword FROM Userprofile"
+			+ " WHERE username = '" + username+"'";			
+		
+		try {
+		   DatabaseTransaction t = DatabaseTransaction.getInstance();
+		   rs = t.getResultset(sql);
+		   if(rs.next()) {
+ 		   	pswd = rs.getString(1);
+ 		   }
+		}catch (SQLException sqlex) {
+			sqlex.printStackTrace();
+		}  
+		
+		return pswd;
 	}
 
 	/**
@@ -64,16 +113,47 @@ public class AccessManager
 	 * authentication fail.
 	 */
 
-	public boolean authenticate(String username, String pw)
+	public boolean authenticate(String username, String pw, DatabaseTransaction t) throws FlexDatabaseException
 	{
-		boolean isCorrect = false;
-		//To be determined by use case
-
-		return isCorrect;
+		String sql = "select * from userprofile\n"+
+					 "where username='"+username+"'\n"+
+					 "and userpassword='"+pw+"'";
+		Vector v = t.executeSql(sql);
+		if(v.size() == 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
-	public void addUser()
+	/**
+	 * This method adds a new user to the database
+	 * pre-condition: none of the parameters contain null values
+	 * also, the username is unique (not identical with any
+	 * existing usernames)
+	 *
+	 * @param username.
+	 * @param pswd the user password
+	 * @param email
+	 * @param group the user group
+	 * @param org the user organization
+	 * @param info the user contact info.
+	 */
+
+	public void addUser(String username, String pswd, String email,String group, String org, String info)
 	{
+		String sql;	
+		
+
+	//	try {
+	//	   FlexConnector f = new FlexConnector();
+	//	   rs = f.getResultSet(sql);
+	//	    
+	//	}catch (SQLException sqlex) {
+	//		sqlex.printStackTrace();
+	//	}
+
+
 		//sql code to insert a row into userprofile table
 	}
 
@@ -87,4 +167,20 @@ public class AccessManager
 		//To be determined by use case
 	}
 
+	//**************************************************************//
+	//						Test									//
+	//**************************************************************//
+	public static void main(String[] args) {
+		AccessManager manager = AccessManager.getInstance();
+		try {
+			DatabaseTransaction t = DatabaseTransaction.getInstance();
+			if(manager.authenticate("Larry Shumway", "three", t))
+				System.out.println("Testing method authenticate - OK");
+			else
+				System.out.println("Testing method authenticate - ERROR");
+		} catch (FlexDatabaseException e) {
+			System.out.println(e);
+		}
+	}
+	
 } // AccessManager
--- flex/ApplicationCode/Java/User/User.java
+++ flex/ApplicationCode/Java/User/User.java
@@ -1,12 +1,24 @@
-//package flex.core;
+package flex.ApplicationCode.Java.User;
+
+import java.sql.*;
+import java.util.*;
+import java.math.BigDecimal;
+import flex.ApplicationCode.Java.database.*;
+import flex.ApplicationCode.Java.process.Request;
+import flex.ApplicationCode.Java.core.*;
+import flex.ApplicationCode.Java.util.*;
 
 /**
- * This class represents User.
+ * This class represents users.
  * @author Wendy Mar 
+ *
+ * Revision:	05-03-2001 by dzuo
+ *				Added the method to get the customer requests and tested.
  */
 public class User {
-	private int id;
+	private String name;
 	private String email;
+	private String password;
 	private String group;
 	private String organization;
 	private String information;
@@ -14,24 +26,38 @@ public class User {
 	/**
 	 * Constructor. Return an User object.
 	 *
-	 * @param id the User ID.
+	 * @param name the User name.
 	 * @param email the user email address. 
 	 * @param group the user group category.
 	 * @return An User object.
 	 */
-	public User (int id, String email, String group) {
-		this.id = id;
+	public User (String name, String email, String group) {
+		this.name = name;
 		this.email = email;
 		this.group = group;
 	}
 
 	/**
-	 * Return UserID as an int.
+	 * Constructor. Return an User object.
 	 *
-	 * @return A UserID as integer.
+	 * @param name the User name.
+	 * @param email the user email address. 
+	 * @param password the user password.
+	 * @return An User object.
 	 */
-	public int getUserID() {
-		return id;
+	public User (String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+
+
+	/**
+	 * Return Username.
+	 *
+	 * @return A Username as String.
+	 */
+	public String getUsername() {
+		return name;
 	}
 
 	/**
@@ -53,12 +79,40 @@ public class User {
 	}
 
 	/**
-	 * Set user ID.
-	 *
-	 * @param userid The UserID as integer.
+	 * Return User group as a String.
+	 * Pre-condition: The username is valid.
+	 * @param name the username
+	 * @return The user group category.
 	 */
-	public void setUserID(int userid) {
-		id = userid;
+	public String getUserGroup(DatabaseTransaction t) throws FlexDatabaseException   
+	{
+		String sql = "SELECT usergroup FROM userprofile" +
+				 " WHERE username = '" + name + "'";
+		Vector v = t.executeSql(sql);
+		Enumeration enum = v.elements();
+		if(enum.hasMoreElements()) {
+			Hashtable h = (Hashtable)enum.nextElement();
+			group = (String)h.get("USERGROUP");
+		}
+		return group;
+	}
+
+	/**
+	 * Return User information such as contact info.
+	 *
+	 * @return the user contact information.
+	 */
+	public String getUserInfo() {
+		return information;
+	}
+
+	/**
+	 * Set Username.
+	 *
+	 * @param newname The Username as String.
+	 */
+	public void setUserName(String newname) {
+		name = newname;
 	}
 
 	/**
@@ -78,5 +132,85 @@ public class User {
 	public void setUserGroup(String usergroup) {
 		group = usergroup;
 	}
-	
+
+	/**
+	 * Get user requested sequences.
+	 *
+	 * @param t The DatabaseTransaction object.
+	 * @return A Vector of Request objects.
+	 * @exception FlexDatabaseException.
+	 */
+	public Vector getRequests(DatabaseTransaction t) throws FlexDatabaseException {
+		Vector requests = new Vector();
+		String sql = "select requestid, username, "+
+					 "to_char(requestdate, 'fmYYYY-MM-DD') as requestdate\n"+ 
+					 "from request where username='"+name+"'";
+		Vector v = t.executeSql(sql);
+		Enumeration enum = v.elements();
+		while(enum.hasMoreElements()) {
+			Hashtable h = (Hashtable)enum.nextElement();
+			int id = ((BigDecimal)h.get("REQUESTID")).intValue();
+			String username = (String)h.get("USERNAME");
+			String requestdate = (String)h.get("REQUESTDATE");
+			
+			sql = "select sequenceid from requestsequence\n"+
+				  "where requestid = "+id;
+			Vector results = t.executeSql(sql);
+			Enumeration e = results.elements();
+			Vector sequences = new Vector();
+			while (e.hasMoreElements()) {
+				Hashtable result = (Hashtable)e.nextElement();
+				int seqid = ((BigDecimal)result.get("SEQUENCEID")).intValue();
+				FlexSequence sequence = new FlexSequence(seqid);
+				sequences.addElement(sequence);
+			}
+			Request r = new Request(id, username, requestdate, sequences);
+			requests.addElement(r);
+		}
+		
+		return requests;
+	}	
+
+	//**********************************************************//
+	//						Test								//
+	//**********************************************************//
+	public static void main(String [] args) {
+		try {
+			User user = new User("Allison Halleck", "password");
+			DatabaseTransaction t = DatabaseTransaction.getInstance();
+			String group = user.getUserGroup(t);
+			if(group.equals("internal"))
+				System.out.println("Testing getUserGroup - OK");
+			else
+				System.out.println("Testing getUserGroup - ERROR");
+				
+			int requestid = FlexIDGenerator.getID("requestid");
+			t.executeSql("insert into species values('Test')");
+			t.executeSql("insert into flexstatus values('TEST')");
+			t.executeSql("insert into request values ("+requestid+",'Allison Halleck',sysdate)");			
+
+			for(int i=0; i<5; i++) {
+				int sequenceid = FlexIDGenerator.getID("sequenceid");	
+				t.executeSql("insert into flexsequence (sequenceid, flexstatus, genusspecies, dateadded) values ("+sequenceid+",'TEST','Test', sysdate)");
+				t.executeSql("insert into requestsequence values("+requestid+","+sequenceid+")");	
+			}		
+			Vector requests = user.getRequests(t);	
+			Enumeration en = requests.elements();
+			while(en.hasMoreElements()) {
+				Request request = (Request)en.nextElement();
+				System.out.println("Get User request:");
+				System.out.println(request.getId());
+				System.out.println(request.getUsername());
+				System.out.println(request.getDate());
+				Vector sequences = request.getSequences();
+				Enumeration enum = sequences.elements();
+				while(enum.hasMoreElements()) {
+					FlexSequence sequence = (FlexSequence)enum.nextElement();
+					System.out.println(sequence.getId());
+				}
+			}
+		} catch (FlexDatabaseException e) {
+			System.out.println(e);
+		}
+	}
 }
