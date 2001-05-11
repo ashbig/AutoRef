741660214693f59bf9ace3a49f0dab5c3a167055
dongmei_zuo <dongmei_zuo>
2001-05-11 19:23:58 +0000

    Initial import.

---
+++ flex031403/ApplicationCode/Java/blast/TestParser.java
@@ -0,0 +1,33 @@
+/*
+ * TestParser.java
+ *
+ * Created on April 26, 2001, 4:32 PM
+ */
+
+/**
+ *
+ * @author  twei
+ * @version 
+ */
+
+package flex.ApplicationCode.Java.blast;
+
+public class TestParser extends Object {
+    /**
+    * @param args the command line arguments
+    */
+    public static void main (String args[]) {
+        BlastParser parser; 
+        for (int i=0; i<args.length; i++) {
+            try {
+                parser = new BlastParser(args[i]);
+                parser.parseBlast();
+                parser.displayParsed();
+
+            } catch (ParseException e) {
+                System.out.println(e.toString()); 
+            }
+        }
+    }
+
+}
