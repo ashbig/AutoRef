357e16f86d0db988f77e2b7da92143a4f644332b
dongmei_zuo <dongmei_zuo>
2001-05-11 16:25:47 +0000

    Added set protocol method

--- flex031403/ApplicationCode/Java/process/QueueItem.java
+++ flex031403/ApplicationCode/Java/process/QueueItem.java
@@ -1,5 +1,5 @@
 /*
- * $Id: QueueItem.java,v 1.2 2001-04-25 18:37:59 dongmei_zuo Exp $
+ * $Id: QueueItem.java,v 1.3 2001-05-11 16:25:47 dongmei_zuo Exp $
  *
  * File     : QueueItem.java 
  * Date     : 04162001
@@ -12,46 +12,56 @@ package flex.ApplicationCode.Java.process;
  * This class represents an item on a queue.
  */
 public class QueueItem {
-	private Object item;
-	private String date;
-	private Protocol protocol;
-
-	/**
-	 * Constructor.
-	 *
-	 * @param item The item object to be set to.
-	 * @param date The date to be set to.
-	 *
-	 * @return A QueueItem object.
-	 */
-	public QueueItem(Object item, Protocol protocol, String date) {
-		this.item = item;
-		this.date = date;
-		this.protocol = protocol;
-	}
-
-	/**
-	 * Return the item field of this object.
-	 *
-	 * @return The item field of this object.
-	 */
-	public Object getItem() {
-		return item;
-	}
-
-	/**
-	 * Return the protocol of this object.
-	 *
-	 * @return The protocol field of this object.
-	 */
-	public Protocol getProtocol() {
-		return protocol;
-	}
-
-	/**
-	 * Return the date field of this object.
-	 */
-	public String getDate() {
-		return date;
-	}
+    private Object item;
+    private String date;
+    private Protocol protocol;
+    
+    /**
+     * Constructor.
+     *
+     * @param item The item object to be set to.
+     * @param date The date to be set to.
+     *
+     * @return A QueueItem object.
+     */
+    public QueueItem(Object item, Protocol protocol, String date) {
+	this.item = item;
+	this.date = date;
+	this.protocol = protocol;
+    }
+    
+    /**
+     * Return the item field of this object.
+     *
+     * @return The item field of this object.
+     */
+    public Object getItem() {
+	return item;
+    }
+    
+    /**
+     * Return the protocol of this object.
+     *
+     * @return The protocol field of this object.
+     */
+    public Protocol getProtocol() {
+	return protocol;
+    }
+    
+    /**
+     * Sets the protocol for this object
+     *
+     * @param protocol The protocol this object should reference
+     */
+    public void setProtocol(Protocol protocol) {
+	this.protocol = protocol;
+    }
+    /**
+     * Return the date field of this object.
+     */
+    public String getDate() {
+	return date;
+    }
 }
+
+
