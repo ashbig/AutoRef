7dea588ae5e230d338011a7506f2a66b827d9fc9
dongmei_zuo <dongmei_zuo>
2001-05-24 17:50:35 +0000

    Modified to use libraries from dev environment rather than tomcat

--- flex031403/build.bat
+++ flex031403/build.bat
@@ -1,14 +1,14 @@
 @echo off
 rem build.bat -- Build Script for the FLEX Application
-rem $Id: build.bat,v 1.1 2001-05-24 17:04:38 dongmei_zuo Exp $
+rem $Id: build.bat,v 1.2 2001-05-24 17:50:35 dongmei_zuo Exp $
 
 set _CP=%CP%
 set FLEX_TOMCAT_HOME=..\jakarta-tomcat-3.2.1
 
 rem Identify the custom class path components we need
-set CP=%FLEX_TOMCAT_HOME%\lib\ant.jar;%FLEX_TOMCAT_HOME%\lib\servlet.jar
-set CP=%CP%;%FLEX_TOMCAT_HOME%\lib\jaxp.jar;%FLEX_TOMCAT_HOME%\lib\parser.jar
-set CP=%CP%;%JAVA_HOME%\lib\tools.jar
+set CP=.\lib\ant.jar;.\lib\servlet.jar
+set CP=%CP%;.\lib\jaxp.jar;.\lib\parser.jar
+set CP=%CP%;.\lib\tools.jar;.\lib\xerces.jar
 set CP=%CP%;.\lib\classes12.zip;.\lib\jdbc2_0-stdext.jar;.\lib\jmxri.jar;.lib\jmxtools.jar;.\lib\jta.jar;.\lib\poolman.jar;xerces.jar;.\lib\rowset.jar
 
 rem Execute ANT to perform the requird build target
