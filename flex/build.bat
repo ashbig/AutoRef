8291a93e5035994f2284315fecd3f7e9b58d14c2
dongmei_zuo <dongmei_zuo>
2001-05-25 12:30:33 +0000

    Added struts.jar to classpath

--- flex/build.bat
+++ flex/build.bat
@@ -1,6 +1,6 @@
 @echo off
 rem build.bat -- Build Script for the FLEX Application
-rem $Id: build.bat,v 1.3 2001-05-25 10:45:31 dongmei_zuo Exp $
+rem $Id: build.bat,v 1.4 2001-05-25 12:30:33 dongmei_zuo Exp $
 
 set _CP=%CP%
 set FLEX_TOMCAT_HOME=..\jakarta-tomcat-3.2.1
@@ -9,7 +9,7 @@ set TOMCAT_HOME=%FLEX_TOMCAT_HOME%
 rem Identify the custom class path components we need
 set CP=.;.\lib\ant.jar;%FLEX_TOMCAT_HOME%\lib\servlet.jar;
 set CP=%CP%;%FLEX_TOMCAT_HOME%\lib\jaxp.jar;%FLEX_TOMCAT_HOME%\lib\parser.jar;
-set CP=%CP%;.\lib\xerces.jar;
+set CP=%CP%;.\lib\xerces.jar;.\lib\struts.jar
 set CP=%CP%;.\lib\classes12.zip;
 set CP=%CP%;.\lib\jdbc2_0-stdext.jar;
 set CP=%CP%;.\lib\jmxri.jar;.\lib\jmxtools.jar;.\lib\jta.jar;
