09bfc2a9184f9b78d3c559a19fd2bd9e102fd7b3
wenhong_mar <wenhong_mar>
2001-05-25 18:30:47 +0000

    add "throws FlexDatabaseException" to all three methods

--- flex031403/src/edu/harvard/med/hip/flex/util/PrimerCalculator.java
+++ flex031403/src/edu/harvard/med/hip/flex/util/PrimerCalculator.java
@@ -1,14 +1,17 @@
 /**
  *   Interface used for oligo primer calculation
- *   Id: PrimerCalculator.java $
+ *   $Id: PrimerCalculator.java,v 1.3 2001-05-25 18:30:47 wenhong_mar Exp $
  *
  *   @File     	: PrimerCalculator.java 
  *   @Date     	: 04162001 
  *   @author	: Wendy Mar
+ *
+ *   modified 5/25/01 wmar
  */
  
 package edu.harvard.med.hip.flex.util;
 import edu.harvard.med.hip.flex.core.*;
+import edu.harvard.med.hip.flex.database.*;
  
 public interface PrimerCalculator
 {
@@ -17,21 +20,21 @@ public interface PrimerCalculator
 	 * @param sequence A Sequence object
 	 * @return An Oligo object   
  	 */
-	 public Oligo calculateFivepOligo (Sequence sequence);
+	 public Oligo calculateFivepOligo (Sequence sequence) throws FlexDatabaseException;
 	
 	 /**
 	 * Calculate the three prime closed oligo of a given sequence
 	 * @param sequence A Sequence object
 	 * @return An Oligo object   
  	 */
-	 public Oligo calculateThreepCloseOligo(Sequence sequence);
+	 public Oligo calculateThreepCloseOligo(Sequence sequence) throws FlexDatabaseException;
 
 	 /**
 	 * Calculate the three prime open oligo of a given sequence
 	 * @param sequence A Sequence object
 	 * @return An Oligo object   
  	 */
-	 public Oligo calculateThreepOpenOligo(Sequence sequence);
+	 public Oligo calculateThreepOpenOligo(Sequence sequence) throws FlexDatabaseException;
 
 }
 
