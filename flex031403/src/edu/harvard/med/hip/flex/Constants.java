31683ab864dd89f2b7f965e1202fecf9a6c7805d
dongmei_zuo <dongmei_zuo>
2001-05-30 14:53:41 +0000

    added flex.name

--- flex031403/src/edu/harvard/med/hip/flex/Constants.java
+++ flex031403/src/edu/harvard/med/hip/flex/Constants.java
@@ -1,5 +1,5 @@
 /*
- * Constants.java
+ * 
  *
  * Created on May 25, 2001, 11:03 AM
  */
@@ -12,8 +12,11 @@ package edu.harvard.med.hip.flex;
  * @version 
  */
 public class Constants {
+    // key used to find user object in session.
     public static final String USER_KEY = "USER";
     
+    // key used to find The flex sequence id.
+    public static final String FLEX_SEQUENCE_KEY = "FLEX_SEQUENCE";
     /** Creates new Constants */
     private Constants() {
     }
