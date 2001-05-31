57820a5242e4eafb3b33a38d550d5baef33b945c
dongmei_zuo <dongmei_zuo>
2001-05-31 11:53:04 +0000

    Added keys for view sequence UI

--- flex/src/edu/harvard/med/hip/flex/Constants.java
+++ flex/src/edu/harvard/med/hip/flex/Constants.java
@@ -16,6 +16,12 @@ public class Constants {
     public static final String USER_KEY = "USER";
     
     // key used to find The flex sequence id.
+    public static final String FLEX_SEQUENCE_ID_KEY = "FLEX_SEQUENCE_ID";
+    
+    // key used to find the fasta formated colorized sequence
+    public static final String FASTA_COLOR_SEQUENCE_KEY = 
+        "FASTA_COLOR_SEQUENCE";
+    
     public static final String FLEX_SEQUENCE_KEY = "FLEX_SEQUENCE";
     /** Creates new Constants */
     private Constants() {
