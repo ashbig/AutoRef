c23e17c765407cf104fab7da26b668808b505e59
dongmei_zuo <dongmei_zuo>
2001-06-08 16:01:47 +0000

    no message

---
+++ flex/src/edu/harvard/med/hip/flex/process/PlatesetProcessObject.java
@@ -0,0 +1,69 @@
+/*
+ * PlatesetProcessObject.java
+ *
+ * Created on June 8, 2001, 2:40 PM
+ *
+ * This class represents a plateset object that gets executed during a process.
+ */
+
+package edu.harvard.med.hip.flex.process;
+
+import java.sql.*;
+import edu.harvard.med.hip.flex.database.*;
+
+/**
+ *
+ * @author  dzuo
+ * @version 
+ */
+public class PlatesetProcessObject extends ProcessObject {
+    /**
+     * Constructor.
+     *
+     * @param id The id of the processed object.
+     * @param executionid The execution id of the processed object.
+     * @param iotype The input/output type of the  processed object.
+     *
+     * @return A ProcessObject object.
+     */
+    public PlatesetProcessObject(int id, int executionid, String iotype) {
+        super(id, executionid, iotype);
+    }
+    
+    /**
+     * Insert the record into processobject table.
+     *
+     * @param conn The <code>Connectoin</code> to do the insert with
+     *
+     * @exception FlexDatabaseException.
+     */
+    public void insert(Connection conn)throws FlexDatabaseException {
+        String sql = "insert into processobject "+
+                     "(executionid, inputoutputflag, platesetid) "+
+                     "values("+executionid+",'"+iotype+"',"+id+")";
+        DatabaseTransaction.executeUpdate(sql,conn);
+    }
+
+    //**********************************************************//
+    //              Testing                                     //
+    //**********************************************************//
+    public static void main(String[] args) {
+        PlatesetProcessObject seqobject = new PlatesetProcessObject(1, 28, "I");
+        Connection c = null;
+      
+        try {
+            DatabaseTransaction t = DatabaseTransaction.getInstance();
+            c = t.requestConnection();
+            seqobject.insert(c);
+        } catch (FlexDatabaseException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                c.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+}
+
