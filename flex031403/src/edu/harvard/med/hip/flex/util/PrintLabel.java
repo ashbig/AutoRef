04f9e20b6dc18bf455aba974c45c9e9df0253be4
dongmei_zuo <dongmei_zuo>
2001-07-02 16:45:18 +0000

    no message

--- flex031403/src/edu/harvard/med/hip/flex/util/PrintLabel.java
+++ flex031403/src/edu/harvard/med/hip/flex/util/PrintLabel.java
@@ -19,7 +19,13 @@ import java.io.IOException;
 import java.lang.InterruptedException; 
 
 public class PrintLabel {
-
+       /**
+        * static variables
+        **/
+       static final String printerName = "zebra";
+       static final int labelWidth = 10;
+       static final int labelHeight = 10;
+       
        /** 
         * Creates new PrintLabel 
         */
@@ -27,23 +33,28 @@ public class PrintLabel {
        }
 
        public String execute(String label) {
-           /* 
+           /** 
             * the status message that going to return
-            */
+            **/
            StringBuffer msg = new StringBuffer();
            
 
            try { 
-               /* 
+               /** 
                 * Execute the command using the Runtime object and get the 
                 * Process which controls this command 
-                */
-               String cmd = "perl print_label.pl " + label;
+                **/
+               String cmd = "perl print_label.pl " + label + " " 
+                            + printerName + " " 
+                            + labelWidth + " " 
+                            + labelHeight ;
+               
                Process p = Runtime.getRuntime().exec(cmd); 
                       
-               /* Use the following code to wait for the process to finish 
-                *  and check the return code from the process 
-                */ 
+               /**
+                * Use the following code to wait for the process to finish 
+                * and check the return code from the process 
+                **/ 
                try { 
                     p.waitFor();
 		    
@@ -65,7 +76,9 @@ public class PrintLabel {
                     }
                     
                     p.destroy();
-               /* Handle exceptions for waitFor() */ 
+               /**
+                * Handle exceptions for waitFor() 
+                **/ 
                } catch (InterruptedException intexc) { 
                     msg.append("Interrupted exception from waitfor, detail: " 
                                 + intexc.getMessage()); 
@@ -75,9 +88,9 @@ public class PrintLabel {
                                 + e.getMessage());
                }
 
-           /* 
+           /** 
             * Handle the exceptions for exec() 
-            */ 
+            **/ 
            } catch (IOException e) { 
                msg.append("IO Exception from exec, detail : " 
                                   + e.getMessage()); 
