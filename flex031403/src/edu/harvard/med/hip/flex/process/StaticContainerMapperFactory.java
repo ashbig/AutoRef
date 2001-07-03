f1c10bd719efc5bc7a51081ec2ff72ad9b7e1203
dongmei_zuo <dongmei_zuo>
2001-07-03 17:08:51 +0000

    Initial import.

---
+++ flex031403/src/edu/harvard/med/hip/flex/process/StaticContainerMapperFactory.java
@@ -0,0 +1,36 @@
+/*
+ * StaticContainerMapperFactory.java
+ *
+ * Create different ContainerMapper objects based on the process name.
+ *
+ * Created on July 3, 2001, 2:14 PM
+ */
+
+package edu.harvard.med.hip.flex.process;
+
+/**
+ *
+ * @author  dzuo
+ * @version 
+ */
+public class StaticContainerMapperFactory {
+
+    /** Creates new StaticContainerMapperFactory */
+    public StaticContainerMapperFactory() {
+    }
+
+    /**
+     * Static method to create the new ConainerMapper object.
+     *
+     * @param processname The name of the process.
+     * @return The ContainerMapper object.
+     * @exception FlexProcessException.
+     */
+    public static ContainerMapper makeContainerMapper(String processname) throws FlexProcessException {
+        if(Protocol.GENERATE_AGAR_PLATES.equals(processname)) {
+            return new AgarContainerMapper();
+        } else {
+            return new OneToOneContainerMapper();
+        }
+    }
+}
