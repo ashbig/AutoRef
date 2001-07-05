71ea9f07c9602ebda5bbb89eb7bab0fb532639a7
wenhong_mar <wenhong_mar>
2001-07-05 18:42:53 +0000

    no message

--- flex/src/edu/harvard/med/hip/flex/process/SequenceOligoQueue.java
+++ flex/src/edu/harvard/med/hip/flex/process/SequenceOligoQueue.java
@@ -25,26 +25,36 @@ public class SequenceOligoQueue {
      * Retrieve all the queued sequences which are waiting for
      * oligo primer calculation
      *
-     * @param protocolId The protocol ID.
+     * @param protocol The process protocol.
      * @cdsSize The integer indicating small, medium or large cds size class
      * @return A LinkedList of Sequence objects
      */
     
-    public LinkedList getQueueItems(int protocolId, int cdsSize) throws FlexDatabaseException {
+    public LinkedList getQueueItems(Protocol protocol, int cdsSize) throws FlexDatabaseException {
         //The result list stores a list of sequence objects
         LinkedList result = new LinkedList();
         
         String sql = "SELECT s.sequenceid as seqid, s.cdsstart, s.cdsstop \n"+
         "FROM FLEXSEQUENCE s, QUEUE q\n" +
         "WHERE s.sequenceid = q.sequenceid\n" +
-        "AND q.protocolid = "+ protocolId + "\n" +
+        "AND q.protocolid = "+ protocol.getId() + "\n" +
         "AND s.cdslength <= " + cdsSize;
         result = restore(sql);
         
         return result; //return the LinkedList
     } // getQueueItems
     
-        public LinkedList getQueueItems(int protocolId, int cdsSizeLowerLimit, 
+    
+    /**
+     * Retrieve all the queued sequences which are waiting for
+     * oligo primer calculation (construct design)
+     *
+     * @param protocol The process protocol.
+     * @cdsSizeLowerLimit The integer indicates the lower limit of the cds size class
+     * @cdsSizeUpperLimit The integer indicates the upper limit of the cds size class
+     * @return A LinkedList of Sequence objects
+     */
+        public LinkedList getQueueItems(Protocol protocol, int cdsSizeLowerLimit, 
          int cdsSizeUpperLimit) throws FlexDatabaseException {
         //The result list stores a list of sequence objects
         LinkedList result = new LinkedList();
@@ -52,7 +62,7 @@ public class SequenceOligoQueue {
         String sql = "SELECT s.sequenceid as seqid, s.cdsstart, s.cdsstop \n"+
         "FROM FLEXSEQUENCE s, QUEUE q\n" +
         "WHERE s.sequenceid = q.sequenceid\n" +
-        "AND q.protocolid = "+ protocolId + "\n" +
+        "AND q.protocolid = "+ protocol.getId() + "\n" +
         "AND s.cdslength >= " + cdsSizeLowerLimit + "\n" +
         "AND s.cdslength < " + cdsSizeUpperLimit;
         result = restore(sql);
@@ -64,19 +74,19 @@ public class SequenceOligoQueue {
      * Retrieve the batch of queued items which are waiting for the
      * next workflow process on a particular date from the Queue table
      *
-     * @param protocolId The protocol ID.
+     * @param protocol The protocol.
      * @param date The date added to the queue in yyyy-mm-dd format.
      * @return A LinkedList of sequence objects.
      * @exception FlexDatabaseException.
      */
-    public LinkedList getQueueItems(int protocolId, String date) throws FlexDatabaseException {
+    public LinkedList getQueueItems(Protocol protocol, String date) throws FlexDatabaseException {
         
         LinkedList result = new LinkedList();
         String sql = "SELECT s.sequenceid as seqid, s.cdsstart, s.cdsstop"+
         "to_char(q.dateadded, 'fmYYYY-MM-DD') as dateadded\n" +
         "FROM FLEXSEQUENCE s, QUEUE q\n" +
         "WHERE s.sequenceid = q.sequenceid\n" +
-        "AND q.protocolid = "+ protocolId + ",\n" +
+        "AND q.protocolid = "+ protocol.getId() + ",\n" +
         "AND to_char(dateadded, 'fmYYYY-MM-DD') = '"+date+"'";
         
         
@@ -92,12 +102,12 @@ public class SequenceOligoQueue {
      * @param c The database Connectin object.
      * @exception FlexDatabaseException.
      */
-    public void removeQueueItems(LinkedList seqList, int protocolId, Connection c) throws FlexDatabaseException {
+    public void removeQueueItems(LinkedList seqList, Protocol protocol, Connection c) throws FlexDatabaseException {
         if (seqList == null)
             return;
         
         String sql = "DELETE FROM queue\n" +
-        "WHERE protocolid = "+ protocolId  + "\n" +
+        "WHERE protocolid = "+ protocol.getId()  + "\n" +
         "AND sequenceid = ?";
         PreparedStatement stmt = null;
         try {
@@ -135,11 +145,11 @@ public class SequenceOligoQueue {
             rs = DatabaseTransaction.getInstance().executeQuery(sql);
             while (rs.next()) {
                 id = rs.getInt(1);
-                System.out.println("sequence ID from rs: "+id);
                 start = rs.getInt(2);
                 stop = rs.getInt(3);
                 seq = new Sequence(id,start,stop);
                 itemlist.add(seq);
+                System.out.println("sequence ID read from queue: "+id);
             } // while
             
         } catch (SQLException sqlex) {
@@ -154,14 +164,14 @@ public class SequenceOligoQueue {
     public static void main(String [] args) {
         Connection c = null;
         SequenceOligoQueue queue = new SequenceOligoQueue();
-        int protocolId = 3; //protocol ID for 'Design Construct' is 3
+        
         try {
-            
+            Protocol protocol = new Protocol("design constructs");
             DatabaseTransaction t = DatabaseTransaction.getInstance();
             c = t.requestConnection();
             //get a list of sequence objects
             LinkedList items = new LinkedList();
-            items = queue.getQueueItems(protocolId, 2000);
+            items = queue.getQueueItems(protocol, 2000);
             ListIterator iter = items.listIterator();
             System.out.println("Get small sequences from queue:");
             while (iter.hasNext()) {
