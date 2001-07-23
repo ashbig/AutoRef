e9a88199add74571c462c13af88f3f1f8daf92fb
wendy <wendy>
2001-07-23 16:48:45 +0000

    no message

--- flex/src/edu/harvard/med/hip/flex/process/OligoPlater.java
+++ flex/src/edu/harvard/med/hip/flex/process/OligoPlater.java
@@ -23,8 +23,8 @@ import java.io.*;
 
 public class OligoPlater {
     
-    //private static final String filePath = "/tmp/";
-    private static final String filePath = "H:/Dev/OligoOrder/";
+    private static final String filePath = "/tmp/";
+    //private static final String filePath = "H:/Dev/OligoOrder/";
     
     private static final int positiveControlPosition = 1;
     private static final int negativeControlPosition = 96;
@@ -86,8 +86,7 @@ public class OligoPlater {
         insertProcessOutput();
         
         //insert receive oligo queue
-        insertReceiveOligoQueue();
-        
+        insertReceiveOligoQueue();        
     }
     
     
