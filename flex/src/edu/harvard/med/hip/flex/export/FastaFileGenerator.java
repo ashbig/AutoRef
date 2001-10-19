5db970d3aa6efad7271b92b092823c67b19df04e
dzuo <dzuo>
2001-10-19 17:48:24 +0000

    no message

--- flex/src/edu/harvard/med/hip/flex/export/FastaFileGenerator.java
+++ flex/src/edu/harvard/med/hip/flex/export/FastaFileGenerator.java
@@ -7,6 +7,7 @@
 package edu.harvard.med.hip.flex.export;
 
 import edu.harvard.med.hip.flex.core.*;
+import edu.harvard.med.hip.flex.util.*;
 import edu.harvard.med.hip.flex.database.*;
 import edu.harvard.med.hip.flex.util.Mailer;
 import edu.harvard.med.hip.utility.Logger;
