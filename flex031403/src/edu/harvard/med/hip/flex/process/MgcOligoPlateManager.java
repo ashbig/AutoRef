903a0f176f06bb6427808bf54ad91c328e4b17cb
Elena <Elena>
2002-06-11 19:29:08 +0000

    ht: final changes

--- flex031403/src/edu/harvard/med/hip/flex/process/MgcOligoPlateManager.java
+++ flex031403/src/edu/harvard/med/hip/flex/process/MgcOligoPlateManager.java
@@ -22,12 +22,15 @@ import java.util.*;
 import java.io.*;
 
 
+
 public class MgcOligoPlateManager extends OligoPlateManager
 {
     public static String MGC_PROTOCOL = Protocol.MGC_DESIGN_CONSTRUCTS;
-    
-    private int      m_orderSequencesOutside = -1;
-    
+    protected static final String filePath = "/tmp/";
+    protected static final String DELIM = "\t";
+    private boolean        m_isResortSequences = false;
+    private String         m_plateType = "96 WELL OLIGO PLATE";
+   
     /**
      * Constructor
      * Creates new OligoPlateManager
@@ -36,6 +39,7 @@ public class MgcOligoPlateManager extends OligoPlateManager
     {
         super(conn);        
         this.isGroupBySize = false;
+        this.m_isReorderSequences = false;
     }
     
     /**
@@ -52,14 +56,18 @@ public class MgcOligoPlateManager extends OligoPlateManager
     throws FlexDatabaseException
     {
         super(conn, project, workflow);
-        this.isGroupBySize = false;        
+        this.isGroupBySize = false;    
+        this.m_isReorderSequences = false;
     }
     
     public MgcOligoPlateManager(Connection conn, Project project, Workflow workflow,
     int totalWells, boolean isFull, Protocol protocol)  throws FlexDatabaseException
     {
+        //m_plateType
         super(conn,project,workflow,totalWells,isFull,false,protocol);
+        this.m_isReorderSequences = false;
     }
+    
     
    public MgcOligoPlateManager(Connection conn, Project project, Workflow workflow,
                  boolean isFull, Protocol protocol)  throws FlexDatabaseException
@@ -71,169 +79,178 @@ public class MgcOligoPlateManager extends OligoPlateManager
        this.protocol = new Protocol(MGC_PROTOCOL);
    }
    
-    public void setOrderRequest(int order)
-    {
-        m_orderSequencesOutside = order;
-    }
     
-    public int getOrderRequest()
-    {
-        return m_orderSequencesOutside;
-    }
-            
+    public void                     setOrderRequest(boolean order)   {    m_isResortSequences = order;    }
+    public boolean                      getOrderRequest()  {       return m_isResortSequences;    }
+     
+    /* function overwrites createOligoPlates method for OligoPlater
+     * 1. if only full plates are requered removes trailing sequences;
+     * 2. sort sequences by marker/ number of sequences on container
+     * 3. checks if all culture blocks plates for destination plate are exist
+     * 4. call create OligoPlate per each plate
+     * 5. write robot file per destination plate
+     */
     protected void createOligoPlates(LinkedList seqList) throws FlexDatabaseException, FlexCoreException, IOException, Exception
     {
+       
+        int number_of_destination_plates = 0;
         
-     
-        ArrayList plates = new ArrayList();
-        /*
-        
-        if (m_isOnlyFullPlates)//only full plates
+        if (seqList == null || seqList.size() == 0) return;  
+        if (isOnlyFullPlate)//only full plates: delete trailing sequences
         {
-            int numSeqToProcess = m_totalWells * ( (int)Math.ceil(seqList.size() / m_totalWells));
+            int numSeqToProcess = totalWells * ( (int)Math.ceil(seqList.size() / totalWells));
             if (numSeqToProcess < 0) return;
             int numberOfSequencesToTruncate = seqList.size() - numSeqToProcess;
-            for (int count = 0; count < numberOfSequencesToTruncate; count++)
+            for (int count = 0; count   < numberOfSequencesToTruncate; count++)
             {
                 seqList.removeLast();
             }
         }
+           //arrange sequences by marker / number of sequences on mgc plate
+        // and put them in destination plate groups
+                
+        Rearrayer ra = new Rearrayer(new ArrayList(seqList), totalWells);
+        ArrayList messages = new ArrayList();
+        ArrayList plates = ra.getPlates( messages );
         
-        if (m_DESIGN_CONSTRUCTS.equals(Protocol.MGC_DESIGN_CONSTRUCTS))
+        LinkedList plate_sequences = null;
+        ArrayList plate_sequence_descriptions = null;
+       
+        //finnaly create oligos
+        for (int plate_count = 0; plate_count < plates.size(); plate_count++)
         {
-            //arrange sequences by marker/plate/saw-tooth
-            Rearrayer ra = new Rearrayer(new ArrayList(seqList));
-            ra.findOriginalMgcContainers();
-            ArrayList orderedSeq = ra.getSequencesOrderedByMarkerContainerSTP(m_totalWells);
-            //put them in destination plate groups
-            ArrayList destination_plate  =  new ArrayList();
-            for (int seq_count = 0; seq_count <= orderedSeq.size(); ++seq_count)
-            {
-                 destination_plate.add(orderedSeq.get(seq_count));
-                 if ((seq_count % m_totalWells) == 0 || seq_count == orderedSeq.size() )//plate filled
-                 {
-                     plates.add(destination_plate);
-                     destination_plate  =  new ArrayList();
-                 }
-            }
-      
-            //check if all mgc culture plates for this destination plate 
-            //are available
-            //1. get all available culture blocks and build hashtable of culture - mgc
-            ArrayList mc_available = new ArrayList();
-            ArrayList cultureBlocks = new ArrayList();
-            //!!!!!!!!!!!! define here
-            for (int cont_count = 0; cont_count < cultureBlocks.size(); cont_count++)
-            {
-                MgcContainer mc = MgcContainer.findMGCContainerFromCulture((Container) cultureBlocks.get(cont_count));
-                mc_available.add(mc);
-            }
+            //get label for destination plate
+            ProjectWorkflow flow = new ProjectWorkflow(project.getId(),workflow.getId());
+            String projectCode = flow.getCode();
+            String processcode = protocol.getProcesscode();
+            int threadId = FlexIDGenerator.getID("threadid");
+            String plate_label = Container.getLabel( projectCode, processcode, threadId, null)   ;
+            //list of Sequence objects
+            plate_sequences = (LinkedList)((ArrayList)plates.get(plate_count)).get(0);
+            plate_sequence_descriptions = (ArrayList)( (ArrayList)plates.get(plate_count)).get(1);
+      //new rearrayed container
+            
+            Container rearrayed_cont = createContainer(plate_label, threadId,plate_sequences, plate_sequence_descriptions);
+            createOligoPlate(plate_sequences, rearrayed_cont.getId());
+            File robot_file = createRearrayFile(plate_sequence_descriptions, plate_label);
+            fileList.add( robot_file );
+            //put on queue plates for creating DNA plate & Glycerol stock
+            putPlateOnQueue(rearrayed_cont);
             
             
-            /* destination plate
-             *all culture plate are available
-            //1. design primers & constracts
-            //2. generate rearray file
-            //3.group oligo plates and rearrayed culture block into one plateset
-             */
-        
-        /*
-            boolean plate_ok = false;
-            for (int plate_count = 0; plate_count < plates.size(); plate_count++)
-            {
-                plate_ok = false;
-                for (int seq_count = 0; seq_count < m_totalWells; seq_count++)
-                {
-                }
-                if ( plate_ok )
-                {
-                    createOligoPlate((LinkedList)plates.get(plate_count));
-                    createRearrayFile();
-                }
-            }
         }
-         **/
-    }
-    
-   /** 
-   private  void createOligoPlate(LinkedList seqList) throws FlexDatabaseException, FlexCoreException, IOException, Exception
-    {
-           ConstructGenerator cg = null;
-        OligoPlater plater = null;
-        try
-        {
-            cg = new ConstructGenerator(seqList, conn, project, workflow, protocol);
-            cg.generateOligoAndConstructs();
-            cg.insertProcessInputOutput();
-            cg.insertConstructQueue();
-            LinkedList oligoPatternList = cg.getOligoPatternList();
-
-            //all of the oligo plate header and sample info are inserted in DB
-            //three text files for order oligos will be generated
-            plater = new OligoPlater(oligoPatternList, cg.getConstructList(), m_conn, m_project, m_workflow);
-            ///remove this 
-            plater.generateOligoOrder();
-            plater.removeOrderOligoQueue();
-
-            //remove sequences from queue
-            removeQueueSequence(seqList);
-            DatabaseTransaction.commit(m_conn);
-        } catch(FlexDatabaseException sqlex)
-            {
-                System.out.println(sqlex.getMessage());
-                DatabaseTransaction.rollback(m_conn);
-            } catch(IOException ioe)
-            {
-                System.out.println("Error occurred while writing to oligo order files");
-                System.out.println(ioe.getMessage());
-                DatabaseTransaction.rollback(m_conn);
-            }
-            finally
-            {
-                DatabaseTransaction.closeConnection(m_conn);
-            }
-  
-        m_fileList = plater.generateOligoOrderFiles();
-        
-    } //createOligoAndConstruct
-    */
-    
-    public void sendOligoOrders() throws MessagingException
-    {
-        String to = "htaycher@hms.harvard.edu";
-        String from = "htaycher@hms.harvard.edu";
-        String cc = "flexgene_manager@hms.harvard.edu";
-        String subject = "New Oligo Order";
-        String msgText = "The attached files are our oligo order.\n"+
-        "Thank you!";
-        Mailer.sendMessage(to, from, cc, subject, msgText, fileList);
-    }
-
-    /**
-    public synchronized void orderOligo()
-    {
-        int totalQueue_sequences = 0;
-     
-        LinkedList seqList = getQueueSequence();
-        totalQueue_sequences = seqList.size();
-        System.out.println("There are total of " + totalQueue_sequences + " sequences belong to the same size group in the queue");
-        createOligoPlates(seqList);
-        //avoid sending out empty email without files attached
-        if (fileList.size() >= 1)
-        {
-            sendOligoOrders();
-            System.out.println("Oligo order files have been mailed!");
-            DatabaseTransaction.commit(conn);
-        } //inner if
-        else
-        {
-            System.out.println("File error, no order is mailed!");
-            DatabaseTransaction.rollback(conn);
-        } 
    
+       
     }
-    */
+    
+    
+    //function creates rearray container
+    private Container createContainer(String label, int threadId,LinkedList seq, ArrayList seqDesc)
+    throws FlexDatabaseException, FlexCoreException, FlexWorkflowException, FlexProcessException
+        {
+     
+        Location location = new Location(Location.FREEZER);
+        Container cont = new Container(  m_plateType,  location, label, threadId );
+        LinkedList orgContainers = new LinkedList();
+       int orgContainer_id = -1;
+          //add positive control samples
+        int contId = cont.getId();
+        Vector sampleLineageSet = new Vector();
+        
+        Sample control_positive = new Sample(Sample.CONTROL_POSITIVE,1,contId);
+        cont.addSample(control_positive);
       
+        String type = Sample.getType(protocol.getProcessname());   
+        for (int count = 0; count < seq.size(); count ++)
+        {
+            SequenceDescription sd = (SequenceDescription) seqDesc.get(count);
+            if (orgContainer_id != -1 && orgContainer_id != sd.getContainerDescription().getId() )
+            { //input containers - all original mgc/glycerol containers
+                  //create fake container
+                Container ct= new Container(  sd.getContainerDescription().getId() , null,
+                                                  location, sd.getContainerDescription().getLabel());
+                              
+                orgContainers.add(ct);
+            }
+                
+            Sample s = new Sample(type, count+2, contId);
+            cont.addSample(s);
+            sampleLineageSet.addElement(new SampleLineage(sd.getSampleId(), s.getId()));
+        }
+        Sample control_negative = new Sample(Sample.CONTROL_NEGATIVE,seq.size() + 2,contId);
+        cont.addSample(control_negative);
+        cont.insert(conn);
+       
+       //process sample linerage
+        //rearrayed plate is output container
+        createSampleLinerageForContainer( cont,  orgContainers,   sampleLineageSet );
+        return cont;
+    }
+    
+   // process sample linerage
+   //rearrayed plate is output container
+    private void createSampleLinerageForContainer(Container cont, LinkedList orgContainers,
+                                                  Vector sampleLineageSet )
+                throws FlexDatabaseException, FlexCoreException, FlexWorkflowException, FlexProcessException
+    {
+         //process sample linerage
+        //rearrayed plate is output container
+        LinkedList newContainers = new LinkedList();
+        newContainers.add(cont);
+        String executionStatus = edu.harvard.med.hip.flex.process.Process.SUCCESS;
+       
+        WorkflowManager manager = new WorkflowManager(project, workflow, "ProcessPlateManager");
+        manager.createProcessRecord(executionStatus, protocol, new Researcher(Researcher.SYSTEM),
+                                    null, orgContainers, newContainers,
+                                    null, sampleLineageSet, conn);
+    }
+    //function put on queueu plates for creating DNA plates and Glycerol stock
+    private void putPlateOnQueue(Container cont) throws FlexDatabaseException
+    {
+        QueueItem queueItem = null;
+        LinkedList queueItems = new LinkedList();
+        ContainerProcessQueue containerQueue = new ContainerProcessQueue();
+       
+        Protocol createDNA = new Protocol(Protocol.CREATE_DNA_FROM_REARRAYED_CULTURE);
+        Protocol createGlycerol = new Protocol(Protocol.CREATE_GLYCEROL_FROM_REARRAYED_CULTURE);
+
+
+        //put on queue
+        queueItem = new QueueItem( cont, createDNA, project, workflow);
+        queueItem = new QueueItem( cont, createGlycerol, project, workflow);
+        queueItems.add(queueItem);
+
+        containerQueue.addQueueItems(queueItems, conn);
+       
+    }
+    
+  //function writes robot file
+    //image_id, source plate barcode, sourcewell, destination plate barcode, destination well
+    public File createRearrayFile(ArrayList plate, String label) throws IOException
+    {
+       
+        File      fl =   new File(filePath + "Robot.txt");
+        String temp = null;
+        
+        FileWriter fr =  new FileWriter(fl);
+        for (int count_seq = 0; count_seq < plate.size(); count_seq++)
+        {
+            SequenceDescription sd = (SequenceDescription) plate.get(count_seq);
+            temp = sd.getImageId()+DELIM+ sd.getContainerDescription().getLabel()+DELIM+
+                    sd.getPosition() +DELIM+  label+ DELIM+  (count_seq + 2);
+            System.out.println(temp);
+            fr.write(temp);
+
+        }
+        fr.flush();
+        fr.close();
+        return fl;
+    }
+   
+
+   
+     
+     
     //******************************************************//
     //			Test				//
     //******************************************************//
@@ -249,7 +266,8 @@ public class MgcOligoPlateManager extends OligoPlateManager
             p = new Project(5);
             w = new Workflow(7);
        
-            OligoPlateManager om = new OligoPlateManager(c, p, w);
+            MgcOligoPlateManager om = new MgcOligoPlateManager(c, p, w, 5, false, new Protocol(Protocol.MGC_DESIGN_CONSTRUCTS));
+            om.setIsReorder(false);
             System.out.println("About to start thread");
             om.orderOligo();
             System.out.println("finished calling thread");
