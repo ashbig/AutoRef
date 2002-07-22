b2e9687d8643dcfa0835c0b8a0384794a3aade09
Elena <Elena>
2002-07-22 19:17:58 +0000

    ht: allows to set location for mgc

--- flex/src/edu/harvard/med/hip/flex/action/PrintBarcodeAction.java
+++ flex/src/edu/harvard/med/hip/flex/action/PrintBarcodeAction.java
@@ -30,13 +30,15 @@ import org.apache.struts.util.MessageResources;
 import edu.harvard.med.hip.flex.form.CreateProcessPlateForm;
 import edu.harvard.med.hip.flex.core.Container;
 import edu.harvard.med.hip.flex.util.PrintLabel;
+import edu.harvard.med.hip.flex.database.*;
 
 /**
  *
  * @author  dzuo
- * @version 
+ * @version
  */
-public class PrintBarcodeAction extends ResearcherAction {
+public class PrintBarcodeAction extends ResearcherAction
+{
     /**
      * Process the specified HTTP request, and create the corresponding HTTP
      * response (or forward to another web component that will create it).
@@ -56,14 +58,48 @@ public class PrintBarcodeAction extends ResearcherAction {
     ActionForm form,
     HttpServletRequest request,
     HttpServletResponse response)
-    throws ServletException, IOException {
+    throws ServletException, IOException
+    {
         Vector newContainers = (Vector)request.getSession().getAttribute("EnterSourcePlateAction.newContainers");
-        for(int i=0; i<newContainers.size(); i++) {
+        for(int i=0; i<newContainers.size(); i++)
+        {
             Container c = (Container)newContainers.elementAt(i);
             String status = PrintLabel.execute(c.getLabel());
             //System.out.println("Printing barcode: "+status);
         }
         
-        return (mapping.findForward("success"));   
+        // for mgc plates: update location
+         if (request.getParameter("update_location") != null)
+        {
+            Connection conn = null;
+            Vector containers = (Vector)request.getSession().getAttribute("EnterSourcePlateAction.newContainers");
+            try
+            {
+                DatabaseTransaction t = DatabaseTransaction.getInstance();
+                conn = t.requestConnection();
+                
+                // update the location of the containers.
+                for(int count =0; count < containers.size(); count++)
+                {
+                    Container container = (Container)containers.elementAt(count);
+                    container.updateLocation(container.getLocation().getId(), conn);
+                }
+                // Commit the changes to the database.
+                DatabaseTransaction.commit(conn);
+            }
+            catch (Exception ex)
+            {
+                System.out.println(ex.getMessage());
+                DatabaseTransaction.rollback(conn);
+                request.setAttribute(Action.EXCEPTION_KEY, ex);
+                return (mapping.findForward("error"));
+            } 
+            finally
+            {
+                DatabaseTransaction.closeConnection(conn);
+            }
+        }
+        
+        return (mapping.findForward("success"));
     }
 }
