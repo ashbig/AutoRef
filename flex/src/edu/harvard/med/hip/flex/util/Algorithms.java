cf3066fc02bccd7e8a129601a6bad8ee0db5a6c1
dzuo <dzuo>
2003-06-02 20:41:57 +0000

    no message

--- flex/src/edu/harvard/med/hip/flex/util/Algorithms.java
+++ flex/src/edu/harvard/med/hip/flex/util/Algorithms.java
@@ -7,22 +7,17 @@ import edu.harvard.med.hip.flex.process.*;
 import edu.harvard.med.hip.flex.user.*;
 import java.io.*;
 
-public class Algorithms
-{
+public class Algorithms {
     
     // private static final String filePath = "/tmp/";
     private static final String filePath = "c:/";
     
-    public static ArrayList rearangeSawToothPatternInFlexSequence(ArrayList sequences)
-    {
-        ArrayList result = new ArrayList();
+    public static ArrayList rearangeSawToothPatternInFlexSequence(ArrayList sequences) {
         for (int i=0; i< sequences.size();i++)
             
             //sort array by cds length
-            Collections.sort(sequences, new Comparator()
-            {
-                public int compare(Object o1, Object o2)
-                {
+            Collections.sort(sequences, new Comparator() {
+                public int compare(Object o1, Object o2) {
                     return ((FlexSequence) o1).getCdslength() - ((FlexSequence) o2).getCdslength();
                 }
                 /** Note: this comparator imposes orderings that are
@@ -31,28 +26,16 @@ public class Algorithms
                 {      return false;  }
                 // compare
             } );
-            //get middle element
-            int middle = sequences.size() / 2;
-            for (int count = 0; count < middle; count++)
-            {
-                result.add(sequences.get(count));
-                result.add(sequences.get(middle+count));
-            }
-            //ad last element
-            if (result.size() < sequences.size()) result.add(sequences.get(sequences.size() -1));
             
+            ArrayList result = Algorithms.rearrangeInSawToothPattern(sequences);
             return result;
     }
     
     
-    public static ArrayList rearangeSawToothPatternInSequenceDescription(ArrayList sequences)
-    {
-        ArrayList result = new ArrayList();
+    public static ArrayList rearangeSawToothPatternInSequenceDescription(ArrayList sequences) {
         //sort array by cds length
-        Collections.sort(sequences, new Comparator()
-        {
-            public int compare(Object o1, Object o2)
-            {
+        Collections.sort(sequences, new Comparator() {
+            public int compare(Object o1, Object o2) {
                 return ((SequenceDescription) o1).getCdsLength() - ((SequenceDescription) o2).getCdsLength();
             }
             /** Note: this comparator imposes orderings that are
@@ -63,25 +46,15 @@ public class Algorithms
         } );
         //get middle element
         
-        int middle = sequences.size() / 2;
-        for (int count = 0; count < middle; count++)
-        {
-            result.add(sequences.get(count));
-            result.add(sequences.get(middle+count));
-        }
-        //ad last element
-        if (result.size() < sequences.size()) result.add(sequences.get(sequences.size() -1));
+        ArrayList result = Algorithms.rearrangeInSawToothPattern(sequences);
         return result;
     }
     
-    public static LinkedList rearangeSawToothPatternInOligoPattern(LinkedList sequences)
-    {
+    public static LinkedList rearangeSawToothPatternInOligoPattern(LinkedList sequences) {
         LinkedList result = new LinkedList();
         //sort array by cds length
-        Collections.sort(sequences, new Comparator()
-        {
-            public int compare(Object o1, Object o2)
-            {
+        Collections.sort(sequences, new Comparator() {
+            public int compare(Object o1, Object o2) {
                 return ((OligoPattern) o1).getCDSLength() - ((OligoPattern) o2).getCDSLength();
             }
             /** Note: this comparator imposes orderings that are
@@ -93,8 +66,7 @@ public class Algorithms
         //get middle element
         
         int middle = sequences.size() / 2;
-        for (int count = 0; count < middle; count++)
-        {
+        for (int count = 0; count < middle; count++) {
             result.add(sequences.get(count));
             result.add(sequences.get(middle+count));
         }
@@ -103,61 +75,71 @@ public class Algorithms
         return result;
     }
     
+    /**
+     * This function takes the sorted list of sequences and rearranges it into saw-tooth pattern
+     * @param sequences A sorted list based on CDS length
+     * @return A list rearranged in saw-tooth pattern
+     */
+    public static ArrayList rearrangeInSawToothPattern(List sequences) {
+        ArrayList result = new ArrayList();
+        
+        int middle = sequences.size() / 2;
+        for (int count = 0; count < middle; count++) {
+            result.add(sequences.get(count));
+            result.add(sequences.get(middle+count));
+        }
+        //ad last element
+        if (result.size() < sequences.size()) result.add(sequences.get(sequences.size() -1));
+        
+        return result;                
+    }
     
     /**
-	 * This function takes the string and converts it into a reversed sequence.
-	 * @param seq  A String object
-	 * @return A String object
-	 */
-    public static String reverseString(String str)
-    {
+     * This function takes the string and converts it into a reversed sequence.
+     * @param seq  A String object
+     * @return A String object
+     */
+    public static String reverseString(String str) {
         String copy = "";  // The reversed copy.
         //       from str.length() - 1 down to 0.
-        for ( int i = str.length() - 1;  i >= 0;  i-- )
-        {
+        for ( int i = str.length() - 1;  i >= 0;  i-- ) {
             // Append i-th char of str to copy.
             copy += str.charAt(i);
         }
         return copy;
     }
-    public static String cleanWhiteSpaces(String str)
-    {
+    public static String cleanWhiteSpaces(String str) {
         String copy = "";  // The reversed copy.
         char[] ar =str.toCharArray();
         //       from str.length() - 1 down to 0.
-        for ( int i = 0; i < ar.length;  i++ )
-        {
+        for ( int i = 0; i < ar.length;  i++ ) {
             if (Character.isLetterOrDigit(ar[i]) ) copy += ar[i];
-          
+            
         }
         
         return copy;
     }
     
-    public static String cleanChar(String str, char ch)
-    {
+    public static String cleanChar(String str, char ch) {
         String copy = "";  // The reversed copy.
         char[] ar =str.toCharArray();
         //       from str.length() - 1 down to 0.
-        for ( int i = 0; i < ar.length;  i++ )
-        {
-            if (ar[i] != ch ) 
+        for ( int i = 0; i < ar.length;  i++ ) {
+            if (ar[i] != ch )
                 copy += ar[i];
-          
+            
         }
         
         return copy;
     }
     
     
-    public static String replaceChar(String str, char oldch, char newchar)
-    {
+    public static String replaceChar(String str, char oldch, char newchar) {
         StringBuffer copy = new StringBuffer();  // The reversed copy.
         char[] ar =str.toCharArray();
         //       from str.length() - 1 down to 0.
-        for ( int i = 0; i < ar.length;  i++ )
-        {
-            if (ar[i] == oldch ) 
+        for ( int i = 0; i < ar.length;  i++ ) {
+            if (ar[i] == oldch )
                 copy.append( newchar );
             else
                 copy.append( ar[i]);
@@ -166,12 +148,10 @@ public class Algorithms
         return copy.toString();
     }
     
-    public static ArrayList splitString(String value, String spliter)
-    {
+    public static ArrayList splitString(String value, String spliter) {
         ArrayList res = new ArrayList();
         StringTokenizer st = new StringTokenizer(value, spliter);
-        while(st.hasMoreTokens())
-        {
+        while(st.hasMoreTokens()) {
             String val = st.nextToken().trim();
             res.add( val );
         }
@@ -179,26 +159,69 @@ public class Algorithms
     }
     
     
-   
     
     
-    public static int numberOf(String str, char ch)
-    {
+    
+    public static int numberOf(String str, char ch) {
         int res = 0;
         char[] ar =str.toCharArray();
         //       from str.length() - 1 down to 0.
-        for ( int i = 0; i < ar.length;  i++ )
-        {
+        for ( int i = 0; i < ar.length;  i++ ) {
             if ( ar[i] == ch)
                 res++;
-          
+            
         }
         
         return res;
     }
     
-    public static void main(String args[])
-    {
+    //convert well nomenculature from A10 to int
+    public static int convertWellFromA8_12toInt( String well) {
+        int position = -1;
+        well = well.toLowerCase();
+        int row = (int)well.charAt(0);
+        int column = Integer.parseInt(well.substring(1));
+        int a_value = (int) 'a';
+        int first_char_value = 0;
+        int second_char_value = 0;
+        int row_value = 0;
+        
+        
+        row_value = row - a_value + 1;
+        
+        return (column - 1) * 8 +  row_value ;
+        
+        
+    }
+    //convert well nomenculature from A10 to int
+    public static String convertWellFromInttoA8_12( int well) {
+        String position = null;
+        
+        int a_value = (int) 'A';
+        int row_value = well % 8;
+        int column = (int) well / 8  +1 ;
+        char rowname = (char) (a_value + row_value - 1);
+        String column_string ="";
+        
+        if (row_value == 0 ) {
+            if (column - 1 < 10)
+                return "H" +  "0"+(column -1 );
+            else
+                return "H" +  (column -1);
+        }
+        
+        else {
+            if (column < 10)
+                return "" +  rowname+"0"+column;
+            else
+                return "" +  rowname+column;
+        }
+        
+        
+    }
+    
+    
+    public static void main(String args[]) {
         
         
         String s = "ff f f f f f f fd ffff ";
