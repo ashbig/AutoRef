0412c4c04b2f07f2e8b48fba25a88b1bc16245f7
Elena <Elena>
2003-06-10 15:53:43 +0000

    no message

---
+++ bec/src/edu/harvard/med/hip/bec/util/Test.java
@@ -0,0 +1,27 @@
+/*
+ * Test.java
+ *
+ * Created on June 10, 2003, 12:17 PM
+ */
+
+package edu.harvard.med.hip.bec.util;
+
+/**
+ *
+ * @author  htaycher
+ */
+public class Test
+{
+    
+    /** Creates a new instance of Test */
+    public Test()
+    {
+    }
+    
+      public static void main(String args[])
+    {
+        int i = 40;
+        System.out.println("L");
+      }
+    
+}
