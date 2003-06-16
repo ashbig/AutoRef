a6bd8902aec16c224d2a40b7727c78e64a42341e
yanhui <yanhui>
2003-06-16 15:12:40 +0000

    sort the list by plateid and wellid

--- CloneOrder/src/edu/harvard/med/hip/cloneOrder/core/CloneInfoComparator.java
+++ CloneOrder/src/edu/harvard/med/hip/cloneOrder/core/CloneInfoComparator.java
@@ -16,8 +16,13 @@ public class CloneInfoComparator implements Comparator {
     
     public int compare(Object o1, Object o2) {
         CloneInfo c1 = (CloneInfo)o1;
-        CloneInfo c2 = (CloneInfo)o2;        
-        return (c1.getGeneSymbol().compareToIgnoreCase(c2.getGeneSymbol()));
+        CloneInfo c2 = (CloneInfo)o2;
+        int result1 = c1.getPlateId().compareToIgnoreCase(c2.getPlateId());
+        int result2 = c1.getWellIdPosition().compareToIgnoreCase(c2.getWellIdPosition());
+        if (result1!=0){  
+            return (c1.getPlateId().compareToIgnoreCase(c2.getPlateId()));}
+        else{
+            return (c1.getWellIdPosition().compareToIgnoreCase(c2.getWellIdPosition()));}
     }
 }
     
