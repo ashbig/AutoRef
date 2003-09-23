8c2d5a0cc29badbcdc34e2b4e1aef1c52d0a1afb
dzuo <dzuo>
2003-09-23 17:54:33 +0000

    no message

--- flex/src/edu/harvard/med/hip/flex/process/RearrayManager.java
+++ flex/src/edu/harvard/med/hip/flex/process/RearrayManager.java
@@ -27,8 +27,8 @@ import edu.harvard.med.hip.utility.Logger;
 public class RearrayManager {
     public static final String REARRAYEDPLATETYPE = "96 WELL PLATE";
     public static final String DILIM = "\t";
-    //public static final String FILEPATH = "/tmp/";
-    public static final String FILEPATH = "G:\\";
+    public static final String FILEPATH = "/tmp/";
+    //public static final String FILEPATH = "G:\\";
     public static final String LEFTFILE = "left";
     public static final String DEFAULTLOGFILE = "rearray_log.txt";
     
