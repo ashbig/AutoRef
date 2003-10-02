de6c74e66f91eaa410f22fbab4aa08e58beca413
Elena <Elena>
2003-10-02 19:31:06 +0000

    no message

--- bec/docs/script_cleanup.bat
+++ bec/docs/script_cleanup.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-c:
+d:
 @ cd output
 cd needleoutput
 @ del *.in
@@ -10,4 +10,4 @@ cd ..
 cd ..
 @ cd blastoutput
 @ del *.in
-cd c:\
+cd d:\
--- bec/docs/script_startftp_and_formatdb.bat
+++ bec/docs/script_startftp_and_formatdb.bat
@@ -1,16 +1,16 @@
-@ c:
-@ cd c:\test\
-@ ftp -s:c:\bec\bec\docs\script_ftpcommands.txt 128.103.32.164 
-@ copy humangenes c:\test\Human\genes /y
-@ del c:\test\humangenes
-@ c:\blast_new\formatdb -pF -oT -i C:\test\Human\genes
-@ copy yeastgenes c:\test\Yeast\genes /y
-@ del c:\test\yeastgenes
-@ c:\blast_new\formatdb -pF -oT -i C:\test\Yeast\genes
-@ copy mgcgenes c:\test\MGC\genes /y
-@ del c:\test\mgcgenes
-@ c:\blast_new\formatdb -pF -oT -i C:\test\MGC\genes
-@ copy pseudomonasgenes c:\test\Pseudomonas\genes /y
-@ del c:\test\pseudomonasgenes
-@ c:\blast_new\formatdb -pF -oT -i C:\test\Pseudomonas\genes
+@ d:
+@ cd d:\blast_db
+@ ftp -s:d:\bec\bec\docs\script_ftpcommands.txt 128.103.32.164 
+@ copy humangenes d:\blast_db\Human\genes /y
+@ del d:\blast_db\humangenes
+@ d:\bio_programs\Blast\formatdb -pF -oT -i d:\blast_db\Human\genes
+@ copy yeastgenes d:\blast_db\Yeast\genes /y
+@ del d:\blast_db\yeastgenes
+@ d:\bio_programs\Blast\formatdb -pF -oT -i d:\blast_db\Yeast\genes
+@ copy mgcgenes d:\blast_db\MGC\genes /y
+@ del d:\blast_db\mgcgenes
+@ d:\bio_programs\Blast\formatdb -pF -oT -i d:\blast_db\MGC\genes
+@ copy pseudomonasgenes d:\blast_db\Pseudomonas\genes /y
+@ del d:\blast_db\pseudomonasgenes
+@ d:\bio_programs\Blast\formatdb -pF -oT -i d:\blast_db\Pseudomonas\genes
 @ cd..
