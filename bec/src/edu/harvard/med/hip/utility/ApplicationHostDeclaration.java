711ecd0ec6d4bafc79bddf6d5943ed0012ef9473
Elena <Elena>
2003-10-10 13:34:35 +0000

    the only constant defines the computer where application is currently running allowing first shift between debug/server mode

---
+++ bec/src/edu/harvard/med/hip/utility/ApplicationHostDeclaration.java
@@ -0,0 +1,18 @@
+/*
+ * ApplicationHostDeclaration.java
+ *
+ * Created on October 10, 2003, 10:03 AM
+ */
+
+package edu.harvard.med.hip.utility;
+
+/**
+ *
+ * @author  HTaycher
+ */
+public class ApplicationHostDeclaration
+{
+    
+    public static final boolean   IS_BIGHEAD = true;
+    
+}
