7087e2c3a0ce5982c617c498d684e7b703ca0b8b
Elena <Elena>
2004-04-28 18:36:12 +0000

    trace files renaming

--- bec/src/edu/harvard/med/hip/bec/sampletracking/Mapping/NamingFileEntry.java
+++ bec/src/edu/harvard/med/hip/bec/sampletracking/Mapping/NamingFileEntry.java
@@ -20,14 +20,19 @@ public class NamingFileEntry
     private static final String DILIM = "_";
    
     private int     m_plateid = -1;
+    private String  m_plate_label = null;
     private String  m_wellid = null;
+    private int     m_well_number = -1;
     private int     m_cloneid = -1;
     private int     m_sequenceid = -1;
     private int     m_readnum = -1;
     private String  m_orientation = null;
+    private int     m_read_type = -1;//internal/end read
+    private String  m_file_name = null;
     
     private static String  m_filePath =Constants.getTemporaryFilesPath();
        
+    public NamingFileEntry(){}
         public NamingFileEntry(int cloneid,String orientation,int  plateid, String wellid, int sequenceid,
         int readnum)
         {
@@ -40,13 +45,29 @@ public class NamingFileEntry
 
         }
         
-       
-       public int     getPlateId (){ return m_plateid  ;}
-        public String     getWellId (){ return m_wellid  ;}
+        public String   getPlateLabel(){ return   m_plate_label ;}
+        public int      getWellNumber(){ return      m_well_number;}
+        public int      getReadType(){ return  m_read_type;}
+        public String   getFileName(){ return m_file_name ;}
+        public int      getPlateId (){ return m_plateid  ;}
+        public String   getWellId (){ return m_wellid  ;}
         public int     getCloneID (){ return m_cloneid  ;}
         public int     getSequenceId (){ return m_sequenceid  ;}
         public int     getReadNum (){ return m_readnum  ;}
         public String  getOrientation (){ return m_orientation  ;}
+        
+        
+        public void   setPlateLabel(String v ){     m_plate_label = v ;}
+        public void      setWellNumber(int v ){        m_well_number= v ;}
+        public void      setReadType(int v ){    m_read_type= v ;}
+        public void   setFileName(String v ){   m_file_name = v ;}
+        public void      setPlateId (int v ){   m_plateid  = v ;}
+        public void   setWellId (String v ){   m_wellid  = v ;}
+        public void     setCloneID (int v ){   m_cloneid  = v ;}
+        public void     setSequenceId (int v ){   m_sequenceid  = v ;}
+        public void     setReadNum (int v ){   m_readnum  = v ;}
+        public void  setOrientation (String v ){   m_orientation  = v ;}
+        
         public static String       getOrientation(int v) 
         { 
             if ( v == Constants.ORIENTATION_FORWARD)
@@ -77,6 +98,7 @@ public class NamingFileEntry
         m_filePath = filePath;
         return createNamingFile( file_entries);
     }
+    
     public static File createNamingFile(ArrayList file_entries) throws IOException
     {
         
@@ -106,6 +128,45 @@ public class NamingFileEntry
     }
     
     
-    
+     public static ArrayList sortByPlateLabelWell(ArrayList file_names)
+    {
+           //sort array by cds length
+        Collections.sort(file_names, new Comparator()
+        {
+            public int compare(Object o1, Object o2)
+            {
+                NamingFileEntry bfn1= (NamingFileEntry)o1;
+                NamingFileEntry bfn2= (NamingFileEntry)o2;
+                int compare = bfn1.getPlateLabel().compareTo( bfn2.getPlateLabel());
+                if ( compare != 0 ) return compare;
+                return ( bfn1.getWellNumber() - bfn2.getWellNumber() );
+             }
+            /** Note: this comparator imposes orderings that are
+             * inconsistent with equals. */
+            public boolean equals(java.lang.Object obj)            {      return false;  }
+            // compare
+        } );
+        return file_names;
+    }
+     public static ArrayList sortByPlateIdWell(ArrayList file_names)
+    {
+           //sort array by cds length
+        Collections.sort(file_names, new Comparator()
+        {
+            public int compare(Object o1, Object o2)
+            {
+                NamingFileEntry bfn1= (NamingFileEntry)o1;
+                NamingFileEntry bfn2= (NamingFileEntry)o2;
+                int compare = bfn1.getPlateId() -  bfn2.getPlateId();
+                if ( compare != 0 ) return compare;
+                return ( bfn1.getWellNumber() - bfn2.getWellNumber() );
+             }
+            /** Note: this comparator imposes orderings that are
+             * inconsistent with equals. */
+            public boolean equals(java.lang.Object obj)            {      return false;  }
+            // compare
+        } );
+        return file_names;
+    }
     
 }
---
+++ bec/src/edu/harvard/med/hip/bec/sampletracking/Mapping/SequencingFacilityFileName.java
@@ -0,0 +1,130 @@
+/*
+ * BroadFileName.java
+ *
+ * Created on April 22, 2004, 12:19 PM
+ */
+
+package edu.harvard.med.hip.bec.sampletracking.mapping;
+import java.util.*;
+import java.io.*;
+
+import edu.harvard.med.hip.bec.util.*;
+/**
+ *
+ * @author  HTaycher
+ */
+public class SequencingFacilityFileName
+{
+    public final static int      SEQUENCING_FACILITY_BROAD = 1;
+    public final static int      SEQUENCING_FACILITY_AGENCORD = 2;
+   
+     
+    private String          m_file_name = null;
+    private String          m_plate_name = null;
+    private String          m_well_name = null;
+    private int             m_well_number = -1;
+    private String          m_version = null;
+    private String          m_orientation = null;
+    private String          m_extention = null;
+    private boolean         m_isProperName = false;
+    
+    /** Creates a new instance of BroadFileName */
+    public SequencingFacilityFileName(String file_name, int seq_facility_code)
+    {
+         switch ( seq_facility_code)
+         {
+             case SEQUENCING_FACILITY_BROAD : {broadFileName( file_name);break;}
+             case SEQUENCING_FACILITY_AGENCORD :{agencortFileName(file_name);break;}
+         }
+   
+    }
+    
+    
+    
+    public String          getFileName (){ return m_file_name  ;}
+    public String          getPlateName (){ return m_plate_name  ;}
+    public String          getWellName (){ return m_well_name  ;}
+    public int             getWellNumber (){ return m_well_number ;}
+    public String          getVersion (){ return m_version  ;}
+    public String          getOrientation (){ return m_orientation ;}
+    public String          getExtension(){ return m_extention;}
+    public boolean         isWriteFormat (){ return m_isProperName ;}
+
+    public static ArrayList sortByPlateWell(ArrayList file_names)
+    {
+           //sort array by cds length
+        Collections.sort(file_names, new Comparator()
+        {
+            public int compare(Object o1, Object o2)
+            {
+                SequencingFacilityFileName bfn1= (SequencingFacilityFileName)o1;
+                SequencingFacilityFileName bfn2= (SequencingFacilityFileName)o2;
+                int compare = bfn1.getPlateName().compareTo( bfn2.getPlateName());
+                if ( compare != 0 ) return compare;
+                return ( bfn1.getWellNumber() - bfn2.getWellNumber() );
+             }
+            /** Note: this comparator imposes orderings that are
+             * inconsistent with equals. */
+            public boolean equals(java.lang.Object obj)            {      return false;  }
+            // compare
+        } );
+        return file_names;
+    }
+    
+    
+    ///D248P100FA1.T0.scf
+     private  void broadFileName(String broad_file_name)
+     {
+        int plate_name_end_f = 0; int plate_name_end_r = 0;
+        int well_info_start = 0;
+        m_file_name = broad_file_name;
+        
+        plate_name_end_f = broad_file_name.indexOf("F");
+        if ( plate_name_end_f == -1 )
+        {
+            plate_name_end_r = broad_file_name.indexOf("R");
+            m_orientation = "R";
+            m_plate_name = broad_file_name.substring(0, plate_name_end_r).toUpperCase();
+            well_info_start = 1 + plate_name_end_r;
+        }
+        else
+        {
+            m_orientation = "F";
+            m_plate_name = broad_file_name.substring(0, plate_name_end_f).toUpperCase();
+            well_info_start = 1 + plate_name_end_f;
+        }
+        
+        m_well_name = broad_file_name.substring(well_info_start, broad_file_name.indexOf('.'));
+        m_well_number = Algorithms.convertWellFromA8_12toInt(m_well_name);
+        m_version = broad_file_name.substring(broad_file_name.indexOf('.') + 1, broad_file_name.lastIndexOf('.'));
+        m_extention = broad_file_name.substring(broad_file_name.lastIndexOf('.') + 1);
+    
+        if (m_well_number > 0 && m_plate_name != null &&  m_version != null &&  m_orientation != null)
+            m_isProperName = true;
+    }
+//    PRG000548_A01_F.ab1
+      private  void agencortFileName(String agnc_file_name)
+     {
+         ArrayList items = Algorithms.splitString(agnc_file_name,"_");
+         if ( items.size() != 3) return;
+        m_file_name = agnc_file_name;
+        m_plate_name = (String)items.get(0);
+        m_well_name = (String)items.get(1);
+        m_well_number = Algorithms.convertWellFromA8_12toInt((String)items.get(1));
+        m_extention = agnc_file_name.substring(agnc_file_name.indexOf('.') + 1);
+    
+      //  m_version = ;
+        m_orientation = ((String)items.get(2)).substring(0,1).toUpperCase();
+        if (m_well_number != -1 && ( m_orientation.equalsIgnoreCase("F") ||
+        m_orientation.equalsIgnoreCase("R")))
+             m_isProperName = true;
+
+    }
+      public static void main(String args[])
+    {
+        SequencingFacilityFileName br= new SequencingFacilityFileName(" PRG000548_A01_F.ab1", SequencingFacilityFileName.SEQUENCING_FACILITY_AGENCORD);
+      SequencingFacilityFileName br1= new SequencingFacilityFileName("D248P100FA1.T0.scf", SequencingFacilityFileName.SEQUENCING_FACILITY_BROAD);
+      
+        System.exit(0);
+    }
+}
