d785e17420dfff03b7af9e08c7b7ab4275e6dbc0
dzuo <dzuo>
2004-06-10 15:38:58 +0000

    changed label height to -12.

--- flex/src/edu/harvard/med/hip/flex/util/PrintLabel.java
+++ flex/src/edu/harvard/med/hip/flex/util/PrintLabel.java
@@ -25,7 +25,7 @@ public class PrintLabel {
        static final String printerName = "zebra";
        static final int labelWidth = 25;
        //static final int labelHeight = 15;
-       static final int labelHeight = 6;
+       static final int labelHeight = -12;
        
        /** 
         * Creates new PrintLabel 
