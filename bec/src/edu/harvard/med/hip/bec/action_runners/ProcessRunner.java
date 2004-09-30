c3b195776a2ca57f69b8f5297a58ab0c0d482980
Elena <Elena>
2004-09-30 16:54:41 +0000

    clear items list send error mes by email

--- bec/src/edu/harvard/med/hip/bec/action_runners/ProcessRunner.java
+++ bec/src/edu/harvard/med/hip/bec/action_runners/ProcessRunner.java
@@ -58,11 +58,11 @@ public abstract class ProcessRunner implements Runnable
          
          m_items_type = type;
      }
-        
+    public  String        getItems()   {      return  m_items;    }   
      
      public void run()
      {
-         
+          cleanUpItems();
          if (this instanceof PrimerDesignerRunner)
          {  ((PrimerDesignerRunner)this).run(); }
          else if  (this instanceof PolymorphismFinderRunner)
@@ -87,17 +87,21 @@ public abstract class ProcessRunner implements Runnable
      public abstract String getTitle();
   
     
+       public ArrayList prepareItemsListForSQL()
+        {
+         return    prepareItemsListForSQL(m_items_type, m_items);
      
-      public ArrayList prepareItemsListForSQL()
+       }
+      public ArrayList prepareItemsListForSQL(int items_type, String initial_items)
      {
          ArrayList result = new ArrayList();
-         ArrayList      items = Algorithms.splitString( m_items);
+         ArrayList  items = Algorithms.splitString( initial_items);
          ArrayList cycle_items = new ArrayList();
          int cycle_number = 0; int last_item_in_cycle = 0; int first_item_in_cycle = 0;
           while (last_item_in_cycle < items.size() )
           {
               // get items for cycle
-              switch ( m_items_type)
+              switch ( items_type)
               {
                   case Constants.ITEM_TYPE_PLATE_LABELS:
                   {
@@ -166,12 +170,24 @@ public abstract class ProcessRunner implements Runnable
          try
          {
  //send errors
+            m_error_messages = deleteNullMessages(m_error_messages);
             if (m_error_messages != null && m_error_messages.size()>0)
             {
-                 Mailer.sendMessage(m_user.getUserEmail(), "hip_informatics@hms.harvard.edu",
-                "hip_informatics@hms.harvard.edu", 
-               title+": error messages.", "Errors\n " ,m_error_messages);
-
+                String file_name = Constants.getTemporaryFilesPath() + File.separator + "ErrorMessages"+ System.currentTimeMillis()+".txt";
+                Algorithms.writeArrayIntoFile( m_error_messages, false,  file_name) ;
+             /*try// would not like to pick up function from Algorithms
+                 {
+                    FileWriter fr =  new FileWriter(file_name, false);
+                    for (int count = 0; count < m_error_messages.size(); count++)
+                    {
+                            fr.write((String) m_error_messages.get(count)+"\n");
+                    }
+                    fr.flush();                fr.close();
+                 }       catch(Exception e )         {  }*/
+                Mailer.sendMessageWithAttachedFile(m_user.getUserEmail(), "hip_informatics@hms.harvard.edu",
+                "hip_informatics@hms.harvard.edu",title, 
+                title+"\nPlease error messages for your request in  attached file" , 
+                new File(file_name) );
             }
             if (m_file_list_reports != null && m_file_list_reports.size()>0 )//&& this instanceof ReportRunner)
             {
@@ -186,11 +202,61 @@ public abstract class ProcessRunner implements Runnable
                 if (m_items != null && m_items.length() > 0) text+=" Items processed:\n"+m_items;
                 Mailer.sendMessage(m_user.getUserEmail(), "hip_informatics@hms.harvard.edu",
                 "hip_informatics@hms.harvard.edu", title, text  );
-           }
+            }
      
         }
-        catch(Exception e){}
+        catch(Exception e){   System.out.println(e.getMessage());  
+    }
      }
      
+     
+     private ArrayList deleteNullMessages(ArrayList messages)
+     {
+         ArrayList result = new ArrayList();
+         for ( int count = 0; count < messages.size(); count++)
+         {
+             if (messages.get(count) != null) result.add(messages.get(count) );
+         }
+         return result;
+     }
+     
+     public void cleanUpItems()
+     {
+         char[] items_char = null;
+         StringBuffer final_string = new StringBuffer();
+         if ( m_items_type == Constants.ITEM_TYPE_CLONEID ||
+            m_items_type == Constants.ITEM_TYPE_BECSEQUENCE_ID ||
+            m_items_type == Constants.ITEM_TYPE_FLEXSEQUENCE_ID)
+         {
+             ArrayList items = Algorithms.splitString(m_items);
+             if (items == null || items.size() < 1) return;
+             
+             for (int count = 0; count < items.size(); count++)
+             {
+                 items_char = ((String)items.get(count)).toCharArray();
+                 for ( int char_count = 0; char_count < items_char.length; char_count++)
+                 {
+                     if ( ! Character.isDigit(items_char[char_count]) ) break;
+                     if ( char_count == items_char.length - 1)
+                         final_string.append( (String)items.get(count) + " ");
+                 }
+             }
+             m_items = final_string.toString();
+         
+         }
+     }
     
+     
+     
+     
+      public static void main(String args[]) 
+      {
+            ReportRunner  input = new ReportRunner();
+            input.setItems(" 35284    cloneid  35285      35286      35287      35288      35289      35290      35291      35292      35293      35294      35295      35296      35297      35298      35299      35300      35301      35302      35303      35304      35305      35306      35307      35308      35309      35310      35311      35312      35313      35314      35315      35316      35317      35318      35319      35320      35321      35322      35323      35324      35325      35326      35327      35328      35329      35330      35331      35332      35333      35334      35335      35336      35337      35338      35339      35340      35341      35342      35343      35344      35345      35346      35347      35348      35349      35350      35351      35352      35353      35354      35355      35356      35357      35358      35359      35360      35361      35362      35363      35364      35365      35366      35367      35368      35369      35370      35371      35372      35373      35374      35375       2765       2766       2392 ");
+            input.setItemsType( Constants.ITEM_TYPE_CLONEID);
+            System.out.println( input.getItems() );
+            input.cleanUpItems();
+             System.out.println( input.getItems() );
+     
+      }
 }
