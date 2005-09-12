43cf11eeb5f5fb4174d169eabd1416c574dfaba6
Elena <Elena>
2005-09-12 18:50:03 +0000

    lqreads manipulation

--- bec/docs/ApplicationSetup/Trimming_java_script.java
+++ bec/docs/ApplicationSetup/Trimming_java_script.java
@@ -1,5 +1,5 @@
 
-package docs.ApplicationSetup;
+//package docs.ApplicationSetup;
 
 import java.io.*;
 import java.util.*;
@@ -21,24 +21,49 @@ public class Trimming_java_script {
    // private double
    // private double                          m_score = MIN_AVG_QUAL;
 
-
+/* script call parameters
+ "$clone_path
+ $trim_score $trim_first_base $trim_last_base
+  $lqr_is_use_lqr $lqr_is_delete_lqr //--- value 1 - true, 0 - false
+ $lqr_pass_score $lqr_first_base $lqr_last_base $lqr_min_length
+*/
     public static void main(String arg[])
     {
+        boolean isDebug = true;
         String directory_name = null;
         String os = System.getProperty("os.name");
         boolean isWind = ( os.indexOf("Win") > -1);
-        double score = -1; int first_base = -1; int last_base = -1;
         double min_pbd_val = MIN_PRB_VAL;
-        if ( arg.length == 2)
+        double score =20;int first_base = 20; int last_base = 800;
+        int lqr_is_use_lqr =  0;int lqr_is_delete_lqr =  0;
+            int quality_check_min_read_length = 150;
+            int quality_check_score = 20;
+            int quality_check_first_base = 100;
+            int quality_check_last_base =  700;
+
+        if ( arg.length > 0)
         {
-            score = Double.parseDouble( arg[1]);
-            if ( score <= 0 ) return;
+            if ( arg.length != 10 )return;
+           score = Double.parseDouble( arg[1]);
+             first_base = Integer.parseInt( arg[2]);
+             last_base = Integer.parseInt( arg[3] );
+            lqr_is_use_lqr =  Integer.parseInt( arg[4] );
+             lqr_is_delete_lqr =  Integer.parseInt( arg[5] );
+             quality_check_score =  Integer.parseInt( arg[6] );
+             quality_check_first_base =  Integer.parseInt( arg[7] );
+             quality_check_last_base =  Integer.parseInt( arg[8] );
+             quality_check_min_read_length =  Integer.parseInt( arg[9] );
+
         }
-        if ( arg.length == 3)first_base = Integer.parseInt( arg[2]);
-        if ( arg.length == 4)    last_base = Integer.parseInt( arg[3] );
-
-
-
+ System.out.println("score "+score);
+            System.out.println( "first_base "+first_base);
+System.out.println(          "   last_base "+last_base);
+System.out.println(           " lqr_is_use_lqr "+lqr_is_use_lqr);
+System.out.println(        "     lqr_is_delete_lqr"+lqr_is_delete_lqr );
+System.out.println(        "     quality_check_score "+quality_check_score);
+ System.out.println(       "     quality_check_first_base "+quality_check_first_base);
+ System.out.println(       "     quality_check_last_base "+quality_check_last_base );
+ System.out.println(       "     quality_check_min_read_length "+quality_check_min_read_length );
 
         if (arg.length > 0)// production
         {
@@ -48,36 +73,75 @@ public class Trimming_java_script {
         }
         else
         {
-            //testing
-            score = 10;// Trimming_java_script.MIN_AVG_QUAL;
 
-            first_base = -1; last_base = -1;
-              first_base = 50; last_base = -1;
-             // first_base = -1; last_base =500;
-            //  first_base = 50; last_base = 700;
-            directory_name= "/C/bio/plate_analysis/clone_samples/3594/114275";//\\phd_dir\\6142_E05_3277_4426_F5.ab1.phd.1";
-            if ( isWind )
+            directory_name= "/C/bio/plate_analysis/clone_samples/39133/133229";//\\phd_dir\\6142_E05_3277_4426_F5.ab1.phd.1";
             directory_name = convertUnixFileNameIntoWindowsBasedOnOneLetterApproach( directory_name);
         }
         Trimming_java_script runner = new Trimming_java_script();
-        runner.runTrimming(directory_name, score, first_base, last_base);
+        runner.runTrimming(directory_name, score, first_base, last_base ,
+                         lqr_is_use_lqr ,  lqr_is_delete_lqr ,
+                         quality_check_min_read_length ,
+                         quality_check_score ,
+                         quality_check_first_base ,
+                         quality_check_last_base  );
         System.exit(0);
     }
     //---------------------
 
 
-    public  void  runTrimming(String directory_name, double score, int first_base, int last_base)
+    public  void  runTrimming(String directory_name, double score, int first_base, int last_base,
+                int lqr_is_use_lqr ,  int lqr_is_delete_lqr ,
+                int quality_check_min_read_length ,int quality_check_score ,
+               int quality_check_first_base, int quality_check_last_base )
     {
         try
         {
                 double min_pbd_val = (float)Math.pow( (double)10.0, (double)( -score / 10.0 ) );
                 File phd_dir = new File(directory_name + File.separator +"phd_dir");
                 File [] phdFiles = phd_dir.listFiles();
+                System.out.println(phdFiles.length+" files");
+                boolean isHighQualityRead = false;
+                ArrayList top_elements = null;
+                ArrayList botom_elements= null;ArrayList scored_elements = null;
+                ArrayList trimmed_scored_elements = null;
                 for (int count = 0 ; count< phdFiles.length; count++)
                 {
+                    top_elements = new ArrayList();
+                    botom_elements=  new ArrayList();
+                    scored_elements =  new ArrayList();
                     try
                     {
-                        trimPhdFile(phdFiles[count] ,   first_base,  last_base, score, min_pbd_val);
+                        System.out.println(phdFiles[count].getName());
+                        readPhdFile(phdFiles[count] ,  top_elements,           botom_elements,  scored_elements);
+                        System.out.println("read "+phdFiles[count].getName()+" "+scored_elements.size());
+
+                        isHighQualityRead = checkReadQuality(scored_elements , quality_check_min_read_length , quality_check_score ,
+                                            quality_check_first_base,  quality_check_last_base );
+                        System.out.println("read "+phdFiles[count].getName()+" "+ isHighQualityRead);
+                        if ( lqr_is_use_lqr == 1 && !isHighQualityRead)
+                        {
+                            phdFiles[count].delete();
+                            System.out.println("read "+phdFiles[count].getName()+" deleted");
+                            continue;
+                        }
+                        if ( lqr_is_delete_lqr == 1 && !isHighQualityRead)
+                        {
+                          //  File chromat_file = new File(directory_name + File.separator +"chromat_dir"+ File.separator +phdFiles[count].getName());
+                          //  chromat_file.delete();
+                            phdFiles[count].delete();
+                            continue;
+                        }
+                        trimmed_scored_elements = trimScoredArray(scored_elements, score,   first_base,  last_base, min_pbd_val);
+                        System.out.println("trimmed elm "+phdFiles[count].getName()+" "+trimmed_scored_elements.size());
+
+                        if (trimmed_scored_elements.size() > 50 )
+                        {
+                            writeTrimedPhdFile( top_elements,  trimmed_scored_elements, botom_elements, phdFiles[count].getAbsolutePath());
+
+                        }
+                        else
+                            phdFiles[count].delete();
+
                         System.out.println("phd files :" +phdFiles[count].getName());
                     }
                     catch(Exception e){}
@@ -87,6 +151,92 @@ public class Trimming_java_script {
 
     }
 
+
+
+
+    private  void readPhdFile(File phdFile, ArrayList top_elements,
+                ArrayList botom_elements, ArrayList scored_elements) throws Exception
+    {
+
+        BufferedReader input = null;
+       // BufferedWriter output = null;
+        boolean isInsideRead = false;
+        String line = null; int element_count = 0;
+        ScoredElement element = null;
+        String phdFile_name = null;
+        ArrayList element_entities = null;
+        boolean isReadInTopElement = true;
+        boolean isReadInScoredElement = false;
+        boolean isReadInBottomElement = false;
+        try
+        {
+            phdFile_name = phdFile.getAbsolutePath();
+            input = new BufferedReader(new FileReader(phdFile));
+            while ((line = input.readLine()) != null)
+            {
+                if (  line.indexOf("END_DNA" ) != -1)
+                {
+                    isReadInBottomElement = true;
+                    isReadInScoredElement = false;
+                 }
+
+                if ( isReadInTopElement  )
+                {
+                    top_elements.add(line + LINE_SEPARATOR ) ;
+                }
+                else if ( isReadInScoredElement )
+                {
+                    element_entities = splitString(line);
+                    if ( element_entities.size () == 3 )
+                    {
+                        element = new ScoredElement( element_entities ) ;
+                        scored_elements.add(element );
+                    }
+                }
+                else if   (  isReadInBottomElement)
+                {
+                    botom_elements.add(line + LINE_SEPARATOR);
+                }
+                if ( isReadInTopElement && line.indexOf("BEGIN_DNA" ) != -1)
+                {
+                    isReadInScoredElement = true;
+                    isReadInTopElement = false;
+                }
+
+            }
+            input.close();
+      }
+        catch (Exception e)
+        {
+
+         }
+    }
+
+    private boolean checkReadQuality(ArrayList scored_elements ,
+    int quality_check_min_read_length , int quality_check_score ,
+    int quality_check_first_base,  int quality_check_last_base )
+    {
+        int total_score = 0; int base_number = 0;ScoredElement element = null;
+        if ( scored_elements.size() < quality_check_min_read_length) return false;
+        quality_check_last_base = (quality_check_last_base <= scored_elements.size())? quality_check_last_base : scored_elements.size() ;
+ System.out.println(quality_check_first_base +" "+ quality_check_last_base+" "+scored_elements.size());
+        for (int count = quality_check_first_base; count < quality_check_last_base; count++)
+        {
+
+            element = ( ScoredElement ) scored_elements.get( count);
+            if ( element.getChar() != 'N' || element.getChar() != 'n')
+            {
+                total_score += element.getScore(); base_number++;
+                // System.out.println(total_score +" "+ base_number);
+            }
+
+        }
+         System.out.println(total_score +" "+ base_number);
+        return ( (int) total_score / base_number > quality_check_score);
+    }
+
+
+    /*
     private  void trimPhdFile(File phdFile, int first_base, int last_base, double score, double min_pbd_val) throws Exception
     {
 
@@ -172,11 +322,22 @@ public class Trimming_java_script {
 
          }
     }
-
+*/
     private      ArrayList  trimScoredArray(ArrayList scored_elements, double score,  int first_base, int last_base, double min_pbd_val)
     {
         ArrayList trimmed_scored_elements = new ArrayList();
-        int[] trimmed_coordinates =  trimSequencePhredAlgorithm(scored_elements, score,   first_base,  last_base,  min_pbd_val );
+        int[] trimmed_coordinates = new int[2];
+        if (score > 5)
+           trimmed_coordinates =  trimSequencePhredAlgorithm(scored_elements, score,   first_base,  last_base,  min_pbd_val );
+        else
+        {
+            trimmed_coordinates[0] = first_base;
+            if ( last_base <= first_base) trimmed_coordinates[1] = first_base;
+            else
+            {
+                trimmed_coordinates[1] = (last_base >= scored_elements.size() ) ? last_base : scored_elements.size() ;
+            }
+        }
         for(int count = trimmed_coordinates[0]; count < trimmed_coordinates[1]; count++)
         {
             trimmed_scored_elements.add( scored_elements.get(count) );
@@ -196,6 +357,25 @@ public class Trimming_java_script {
         output.flush();
     }
 
+     private   void          writeTrimedPhdFile(ArrayList top_elements,
+                ArrayList scored_elements, ArrayList botom_elements,
+                String phdFile_name )throws Exception
+     {
+            File phdFile = new File(phdFile_name );
+            BufferedWriter output = new BufferedWriter(new FileWriter(phdFile));
+            for (int count = 0; count < top_elements.size() ; count++)
+            {
+                  output.write( (String)top_elements.get(count) );
+            }
+            writeScoredArray( scored_elements,  output);
+            for (int count = 0; count < botom_elements.size() ; count++)
+            {
+               output.write( (String)botom_elements.get(count) );
+            }
+            output.flush();
+            output.close();
+     }
+
 
 
     private     int[]     trimSequencePhredAlgorithm  (ArrayList scored_elements, double score,  int first_base, int last_base, double min_pbd_val )
@@ -208,11 +388,10 @@ public class Trimming_java_script {
           int count = 0;
           ScoredElement element = null;
           int[] trimmed_coordinates = new int[2];
-
+          last_base = ( last_base == 0)? scored_elements.size() : last_base;
           for(count = first_base; count < last_base; ++count )
           {
             element = (ScoredElement) scored_elements.get(count);
-        System.out.println("first element "+ count+" "+element.getScore()+" "+element.getChar());
             probScore +=  min_pbd_val - (float)Math.pow( (double)10.0, (double)( -element.getScore() / 10.0 ) );
             qualScore += (float)element.getScore();
             if( probScore <= 0.0 )
