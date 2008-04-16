87f22edc0966a9fb5f5aa4c80979b47f961daf5b
dz4 <dz4>
2008-04-16 18:12:48 +0000

    no message

--- flex/src/edu/harvard/med/hip/flex/action/GenerateMultipleGlycerolAction.java
+++ flex/src/edu/harvard/med/hip/flex/action/GenerateMultipleGlycerolAction.java
@@ -17,6 +17,7 @@ import edu.harvard.med.hip.flex.process.OneToManyDirectContainerMapper;
 import edu.harvard.med.hip.flex.process.Protocol;
 import edu.harvard.med.hip.flex.process.Researcher;
 import edu.harvard.med.hip.flex.process.StaticContainerMapperFactory;
+import edu.harvard.med.hip.flex.util.PrintLabel;
 import edu.harvard.med.hip.flex.workflow.Project;
 import edu.harvard.med.hip.flex.workflow.Workflow;
 import edu.harvard.med.hip.flex.workflow.WorkflowManager;
@@ -72,25 +73,25 @@ public class GenerateMultipleGlycerolAction extends ResearcherAction {
         try {
             List labels = Algorithms.splitString(srclabels, null);
             Set newlabels = new TreeSet();
-            for(int i=0; i<labels.size(); i++) {
-                String label = (String)labels.get(i);
+            for (int i = 0; i < labels.size(); i++) {
+                String label = (String) labels.get(i);
                 int index = label.indexOf(Container.DAUGHTER_BARCODE_SEPARATER);
-                if(index != -1) {
+                if (index != -1) {
                     label = label.substring(0, index);
                 }
                 newlabels.add(label);
             }
-            if(newlabels.size()<labels.size()) {
+            if (newlabels.size() < labels.size()) {
                 errors.add("srclabels", new ActionError("error.plate.duplicate"));
                 saveErrors(request, errors);
                 return (new ActionForward(mapping.getInput()));
             }
-            
+
             List containers = Container.findContainers(labels, false, false);
             List missing = findMissingContainers(labels, containers);
-           
+
             if (missing.size() > 0) {
-                String missinglabels = Algorithms.convertStringArrayToString((ArrayList)missing, ",");
+                String missinglabels = Algorithms.convertStringArrayToString((ArrayList) missing, ",");
                 errors.add("srclabels", new ActionError("error.plate.invalid.barcode", missinglabels));
                 saveErrors(request, errors);
                 return (new ActionForward(mapping.getInput()));
@@ -114,13 +115,13 @@ public class GenerateMultipleGlycerolAction extends ResearcherAction {
             Workflow workflow = new Workflow(-1, null, null);
             Protocol protocol = new Protocol(Protocol.GENERATE_MULTIPLE_GLYCEROL);
             ContainerMapper mapper = StaticContainerMapperFactory.makeContainerMapper(Protocol.GENERATE_MULTIPLE_GLYCEROL);
-            ((OneToManyDirectContainerMapper)mapper).setNumOfDest(num);
+            ((OneToManyDirectContainerMapper) mapper).setNumOfDest(num);
             Vector newContainers = mapper.doMapping(new Vector(containers), protocol, project, workflow);
             for (int i = 0; i < newContainers.size(); i++) {
                 Container c = (Container) newContainers.get(i);
                 c.setLocation(new Location(Location.CODE_FREEZER, Location.FREEZER, null));
             }
-          
+
             Vector sampleLineageSet = mapper.getSampleLineageSet();
             String executionStatus = edu.harvard.med.hip.flex.process.Process.SUCCESS;
 
@@ -138,22 +139,29 @@ public class GenerateMultipleGlycerolAction extends ResearcherAction {
                     null, containers, newContainers,
                     null, sampleLineageSet, conn);
 
-            List containerlabelmaps = ((OneToManyDirectContainerMapper)mapper).getContainerlabelmaps();
-            for(int i=0; i<containerlabelmaps.size(); i++) {
-                Containerlabelmap m = (Containerlabelmap)containerlabelmaps.get(i);
+            List containerlabelmaps = ((OneToManyDirectContainerMapper) mapper).getContainerlabelmaps();
+            for (int i = 0; i < containerlabelmaps.size(); i++) {
+                Containerlabelmap m = (Containerlabelmap) containerlabelmaps.get(i);
                 Container.updateContainerlabelmap(m.getRoot(), m.getId(), conn);
             }
-            
+
             DatabaseTransaction.commit(conn);
-            
+
             request.getSession().setAttribute("EnterSourcePlateAction.newContainers", newContainers);
+
+            //print barcode labels for new containers
+            for (int i = 0; i < newContainers.size(); i++) {
+                Container c = (Container) newContainers.elementAt(i);
+                String status = PrintLabel.execute(c.getLabel());
+            }
         } catch (Exception ex) {
             DatabaseTransaction.rollback(conn);
             request.setAttribute(Action.EXCEPTION_KEY, new Exception(ex.getMessage()));
             return mapping.findForward("error");
         } finally {
-            if(conn != null)
+            if (conn != null) {
                 DatabaseTransaction.closeConnection(conn);
+            }
         }
         return (mapping.findForward("success"));
     }
