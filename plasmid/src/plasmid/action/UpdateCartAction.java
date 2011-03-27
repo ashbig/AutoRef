dedce9dc9f2f9e78dd8ddf4cb0aa7088643b0062
dz4 <dz4>
2011-03-27 11:18:08 +0000

    no message

--- plasmid/src/plasmid/action/UpdateCartAction.java
+++ plasmid/src/plasmid/action/UpdateCartAction.java
@@ -8,7 +8,6 @@ package plasmid.action;
 
 import java.util.*;
 import java.io.*;
-import java.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,8 +18,6 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import plasmid.database.*;
-import plasmid.database.DatabaseManager.*;
 import plasmid.Constants;
 import plasmid.coreobject.*;
 import plasmid.form.ViewCartForm;
