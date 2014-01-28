612e119a8369a27bce3efd0529031191418080f6
user2 <user2@10.11.112.231>
2014-01-28 14:49:37 -0500

    Revert ""
    This reverts commit df7038dbd8294975b8b17131be4e76f9099ac968.

--- plasmid/src/plasmid/process/OrderProcessManager.java
+++ plasmid/src/plasmid/process/OrderProcessManager.java
@@ -2091,6 +2091,10 @@ public class OrderProcessManager {
         document.add(PdfEditor.makeSmall("  Lola Yao, (617) 432-2408"));
         document.add(PdfEditor.makeSmall("  dnaresource_finance@hms.harvard.edu"));
         
+        document.add(PdfEditor.makeTitle(" "));
+        document.add(PdfEditor.makeSmallBold("For All Other Inquiries Please Contact:"));
+        document.add(PdfEditor.makeSmall("  Glenn Beeman, (617) 432-7708"));
+        document.add(PdfEditor.makeSmall("  plasmidhelp@hms.harvard.edu"));
         
     }
 
