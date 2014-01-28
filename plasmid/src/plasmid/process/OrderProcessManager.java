33b537cd53633794aa56e8f08f8f61b2e50e119c
user2 <user2@10.11.112.231>
2014-01-28 14:49:56 -0500

    Revert ""
    This reverts commit 0a47324620386cf54c413d1bf2120e992ae4be90.

--- plasmid/src/plasmid/process/OrderProcessManager.java
+++ plasmid/src/plasmid/process/OrderProcessManager.java
@@ -2090,11 +2090,6 @@ public class OrderProcessManager {
         document.add(PdfEditor.makeSmallBold("For Invoice Information Contact:"));
         document.add(PdfEditor.makeSmall("  Lola Yao, (617) 432-2408"));
         document.add(PdfEditor.makeSmall("  dnaresource_finance@hms.harvard.edu"));
-        
-        document.add(PdfEditor.makeTitle(" "));
-        document.add(PdfEditor.makeSmallBold("For All Other Inquiries Please Contact:"));
-        document.add(PdfEditor.makeSmall("  Glenn Beeman, (617) 432-7708"));
-        document.add(PdfEditor.makeSmall("  plasmidhelp@hms.harvard.edu"));
     }
 
     public void printInternalInvoice(OutputStream file, CloneOrder order, Invoice invoice) {
