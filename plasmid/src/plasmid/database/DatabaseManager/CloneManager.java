d35318d6666c52189aabe9df1b30c388ef49d696
ashbig <ashbig@10.119.49.92>
2015-07-08 11:48:38 -0400

    trying to get sam sql working

--- plasmid/src/plasmid/database/DatabaseManager/CloneManager.java
+++ plasmid/src/plasmid/database/DatabaseManager/CloneManager.java
@@ -1462,7 +1462,7 @@ public class CloneManager extends TableManager {
                 bbtube = String.format(bbtube,10,'0');
              try{
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		Connection conn = DriverManager.getConnection("jdbc:sqlserver://sam-20kdx12;1433;integratedSecurity=false;user=sa;password=hst;database=SamManager");
+		Connection conn = DriverManager.getConnection("jdbc:sqlserver://sam-20kdx12:1433;integratedSecurity=false;user=sa;password=hst;database=SamManager");
 		Statement sta = conn.createStatement();
 		String Sql = "select TwoDBarcode from dbo.TubeInfo";
 		ResultSet rs = sta.executeQuery(Sql);
